509ca2f19b2e02383439195dcab561eefbba3822
samirettali <ettali.samir@gmail.com>
2019-05-04 15:09:57 +0200

    Restore zshrc after oh my zsh installation

--- Scripts/configure.sh
+++ Scripts/configure.sh
@@ -32,6 +32,9 @@ zsh -c "$(curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zp
 # oh my zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
+# restore zshrc after oh my zsh installation
+mv ~/.zsh.pre-pre-oh-my-zsh ~/.zshrc
+
 # zsh-plugins
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
