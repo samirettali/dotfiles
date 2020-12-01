b411b471b2509cf733b300564753145457c46cfc
Samir Ettali <ettali.samir@gmail.com>
2020-12-01 15:44:51 +0100

    Changed nvim lsp shortcuts and disabled formatting

--- .config/nvim/lua/_lsp_config.lua
+++ .config/nvim/lua/_lsp_config.lua
@@ -21,29 +21,30 @@ local function custom_attach(client)
   map('n', 'gD',         '<cmd>lua vim.lsp.buf.declaration()<CR>')
   map('n', 'gi',         '<cmd>lua vim.lsp.buf.implementation()<CR>')
   map('n', 'gt',         '<cmd>lua vim.lsp.buf.type_definition()<CR>')
-  map('n', 'gr',         '<cmd>lua vim.lsp.buf.references()<CR>')
-  map('n', 'gs',         '<cmd>lua vim.lsp.buf.signature_help()<CR>')
+  map('n', 'gr',         '<cmd>lua vim.lsp.buf.rename()<CR>')
+  map('n', 'gR',         '<cmd>lua vim.lsp.buf.references()<CR>')
+  map('n', 'gh',         '<cmd>lua vim.lsp.buf.signature_help()<CR>')
   map('n', 'dn',         '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>')
   map('n', 'dp',         '<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>')
+  map('n', '<C-s>',      '<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>')
   map('n', 'K',          '<cmd>lua vim.lsp.buf.hover()<CR>')
   map('n', '<Leader>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>')
-  map('n', '<Leader>r',  '<cmd>lua vim.lsp.buf.rename()<CR>')
   map('n', '<Leader>gw', '<cmd>lua vim.lsp.buf.document_symbol()<CR>')
   map('n', '<Leader>gW', '<cmd>lua vim.lsp.buf.workspace_symbol()<CR>')
   map('n', '<Leader>=',  '<cmd>lua vim.lsp.buf.formatting()<CR>')
   map('n', '<leader>ai', '<cmd>lua vim.lsp.buf.incoming_calls()<CR>')
   map('n', '<leader>ao', '<cmd>lua vim.lsp.buf.outgoing_calls()<CR>')
 
-  vim.cmd [[augroup lsp]]
-  vim.cmd       [[au! BufWritePre <buffer> :lua vim.lsp.buf.formatting_sync()]]
-  vim.cmd [[augroup END]]
+  -- vim.cmd [[augroup lsp]]
+  -- vim.cmd       [[au! BufWritePre <buffer> :lua vim.lsp.buf.formatting_sync()]]
+  -- vim.cmd [[augroup END]]
 
   vim.cmd("setlocal omnifunc=v:lua.vim.lsp.omnifunc")
 end
 
 vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
   vim.lsp.diagnostic.on_publish_diagnostics, {
-    virtual_text = true,
+    virtual_text = false,
     signs = true,
     update_in_insert = false,
   }
