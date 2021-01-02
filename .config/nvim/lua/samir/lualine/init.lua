f7d383a8b334e7d354438623b0fb759e6a817205
Samir Ettali <ettali.samir@gmail.com>
2021-01-02 19:10:15 +0100

    Updated lualine config

--- .config/nvim/lua/samir/lualine/init.lua
+++ .config/nvim/lua/samir/lualine/init.lua
@@ -2,21 +2,23 @@ local lualine = require('lualine')
 
 lualine.sections = {
   lualine_a = { 'mode' },
-  lualine_b = { 'modified' },
+  lualine_b = { 'filename' },
   lualine_c = {  },
   lualine_x = { 'location' },
   lualine_y = { 'progress' },
-  lualine_z = { 'fileformat' },
+  lualine_z = { 'filetype' },
   lualine_diagnostics = {  }
 }
 
 lualine.inactiveSections = {
   lualine_a = {  },
-  lualine_b = {  },
+  lualine_b = { 'filename' },
   lualine_c = {  },
   lualine_x = { 'location' },
   lualine_y = {  },
   lualine_z = {  }
 }
+
+lualine.theme = 'default'
 
 lualine.status()
