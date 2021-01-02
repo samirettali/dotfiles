3032dc972b4c4d7f126c2e8d9f38573e5094c03f
Samir Ettali <ettali.samir@gmail.com>
2021-01-02 19:05:19 +0100

    Corrected mappings

--- .config/nvim/lua/samir/sneak/init.lua
+++ .config/nvim/lua/samir/sneak/init.lua
@@ -1,4 +1,4 @@
-map('n', 'f', '<Plug>Sneak_f')
-map('n', 'F', '<Plug>Sneak_F')
-map('n', 't', '<Plug>Sneak_t')
-map('n', 'T', '<Plug>Sneak_T')
+map('n', 'f', '<Plug>Sneak_f', { noremap = false })
+map('n', 'F', '<Plug>Sneak_F', { noremap = false })
+map('n', 't', '<Plug>Sneak_t', { noremap = false })
+map('n', 'T', '<Plug>Sneak_T', { noremap = false })
