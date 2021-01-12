4bc8596bc53a06a080f42d32bbf9ce978175dba3
Samir Ettali <ettali.samir@gmail.com>
2021-01-12 03:06:19 +0100

    Updated install for stow

--- install.sh
+++ install.sh
@@ -8,37 +8,40 @@
 command -v git >/dev/null 2>&1 || { echo >&2 "Git is required, aborting."; exit 1; }
 command -v zsh >/dev/null 2>&1 || { echo >&2 "Zsh is required, aborting."; exit 1; }
 
-# Dotfiles installation
-git clone --bare https://github.com/samirettali/dotfiles $HOME/.cfg
-function dots {
-   /usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME $@
-}
-dots checkout
-if [ $? = 0 ]; then
-    echo "Checked out config.";
-else
-    echo "Backing up pre-existing dot files.";
-    mkdir -p $HOME/.config-backup
-    dots checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} $HOME/.config-backup/{}
-fi;
-dots checkout
-dots config --local status.showUntrackedFiles no
-
 # Install tmux plugin manager
-git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+if [ ! -d "${HOME}/.tmux/plugins/tpm" ]; then
+  mkdir -p "${HOME}/.tmux/plugins"
+  git -C "${HOME}/.tmux/plugins" https://github.com/tmux-plugins/tpm
+fi
 
 # Zsh plugings
-mkdir -p $HOME/.zsh
-git -C "$HOME/.zsh" clone https://github.com/wwalker/ssh-find-agent
-git -C "$HOME/.zsh" clone https://github.com/zsh-users/zsh-autosuggestions
-git -C "$HOME/.zsh" clone https://github.com/starcraftman/zsh-git-prompt
-git -C "$HOME/.zsh" clone https://github.com/zsh-users/zsh-syntax-highlighting
+ZSH_PLUGINS="${HOME}/.zsh"
 
-if hash stack 2>/dev/null; then
-  echo "Haskell detected, compiling zsh-git-prompt..."
-  cd "${HOME}/.zsh/zsh-git-prompt"
-  stack setup
-  stack install
-else
-  echo "Haskell (stack) is not installed, using normal zsh-git-prompt"
+if [ ! -d "${ZSH_PLUGINS}" ]; then
+  mkdir -p "${ZSH_PLUGINS}"
 fi
+
+if [ ! -d "${ZSH_PLUGINS}/ssh-find-agent" ]; then
+  git -C "${ZSH_PLUGINS}" clone https://github.com/wwalker/ssh-find-agent
+fi
+if [ ! -d "${ZSH_PLUGINS}/zsh-autosuggestions" ]; then
+  git -C "${ZSH_PLUGINS}" clone https://github.com/zsh-users/zsh-autosuggestions
+fi
+if [ ! -d "${ZSH_PLUGINS}/ssh-git-prompt" ]; then
+  git -C "${ZSH_PLUGINS}" clone https://github.com/starcraftman/zsh-git-prompt
+fi
+if [ ! -d "${ZSH_PLUGINS}/ssh-syntax-highlighting" ]; then
+  git -C "${ZSH_PLUGINS}" clone https://github.com/zsh-users/zsh-syntax-highlighting
+fi
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+stow "${DIR}/*"
+
+# if hash stack 2>/dev/null; then
+#   echo "Haskell detected, compiling zsh-git-prompt..."
+#   cd "${HOME}/.zsh/zsh-git-prompt"
+#   stack setup
+#   stack install
+# else
+#   echo "Haskell (stack) is not installed, using normal zsh-git-prompt"
+# fi
