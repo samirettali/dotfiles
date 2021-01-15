51338beaa4e91fe04df56a184062b0cc9f14815d
Samir Ettali <ettali.samir@gmail.com>
2021-01-15 17:31:47 +0100

    Disabled virtual text in nvim lsp

--- common/nvim/.config/nvim/lua/samir/lsp/init.lua
+++ common/nvim/.config/nvim/lua/samir/lsp/init.lua
@@ -51,7 +51,7 @@ end
 
 vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
   vim.lsp.diagnostic.on_publish_diagnostics, {
-    virtual_text = true,
+    virtual_text = false,
     signs = true,
     update_in_insert = false,
   }
