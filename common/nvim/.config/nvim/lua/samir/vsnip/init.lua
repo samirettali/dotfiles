ac6e352aa3cc7eceb317b1135eaac8f1071997f8
Samir Ettali <ettali.samir@gmail.com>
2021-02-21 15:06:44 +0100

    Updated vim-vsnip config

--- common/nvim/.config/nvim/lua/samir/vsnip/init.lua
+++ common/nvim/.config/nvim/lua/samir/vsnip/init.lua
@@ -1,47 +1,25 @@
 -- if vim.fn.PluginLoader('vim-vsnip') < 1 then
 --     return
 -- end
+vim.api.nvim_exec([[
+" Expand
+imap <expr> <C-j>   vsnip#expandable()  ? '<Plug>(vsnip-expand)'         : '<C-j>'
+smap <expr> <C-j>   vsnip#expandable()  ? '<Plug>(vsnip-expand)'         : '<C-j>'
 
-map(
-  "i",
-  "<c-l>",
-  "vsnip#jumpable(1) ? '<Plug>(vsnip-jump-next)' : '<c-l>'",
-  {expr = true}
-)
-map(
-  "s",
-  "<c-l>",
-  "vsnip#jumpable(1) ? '<Plug>(vsnip-jump-next)' : '<c-l>'",
-  {expr = true}
-)
-map(
-  "i",
-  "<c-h>",
-  "vsnip#jumpable(1) ? '<Plug>(vsnip-jump-prev)' : '<c-h>'",
-  {expr = true}
-)
-map(
-  "s",
-  "<c-h>",
-  "vsnip#jumpable(1) ? '<Plug>(vsnip-jump-prev)' : '<c-h>'",
-  {expr = true}
-)
-map(
-  "x",
-  "<c-j>",
-  [[vsnip#available(1)  ? '<Plug>(vsnip-expand-or-jump)' : '<C-j>']],
-  {expr = true}
-)
-map(
-  "i",
-  "<c-j>",
-  [[vsnip#available(1)  ? '<Plug>(vsnip-expand-or-jump)' : '<C-j>']],
-  {expr = true}
-)
-map(
-  "s",
-  "<c-j>",
-  [[vsnip#available(1)  ? '<Plug>(vsnip-expand-or-jump)' : '<C-j>']],
-  {expr = true}
-)
+" Expand or jump
+imap <expr> <C-l>   vsnip#available(1)  ? '<Plug>(vsnip-expand-or-jump)' : '<C-l>'
+smap <expr> <C-l>   vsnip#available(1)  ? '<Plug>(vsnip-expand-or-jump)' : '<C-l>'
 
+" Jump forward or backward
+imap <expr> <Tab>   vsnip#jumpable(1)   ? '<Plug>(vsnip-jump-next)'      : '<Tab>'
+smap <expr> <Tab>   vsnip#jumpable(1)   ? '<Plug>(vsnip-jump-next)'      : '<Tab>'
+imap <expr> <S-Tab> vsnip#jumpable(-1)  ? '<Plug>(vsnip-jump-prev)'      : '<S-Tab>'
+smap <expr> <S-Tab> vsnip#jumpable(-1)  ? '<Plug>(vsnip-jump-prev)'      : '<S-Tab>'
+
+" Select or cut text to use as $TM_SELECTED_TEXT in the next snippet.
+" See https://github.com/hrsh7th/vim-vsnip/pull/50
+nmap        s   <Plug>(vsnip-select-text)
+xmap        s   <Plug>(vsnip-select-text)
+nmap        S   <Plug>(vsnip-cut-text)
+xmap        S   <Plug>(vsnip-cut-text)
+]], false)
