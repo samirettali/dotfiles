a7c1a4de4600b97e56afacfe989282bc02b0edff
Samir Ettali <ettali.samir@gmail.com>
2021-04-28 21:07:47 +0200

    [PROV] Added tools

--- provisioning/tiling.sh
+++ provisioning/tiling.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-sudo pacman --noconfirm --needed xorg-server xorg-xinit xsecurelock xss-lock xf86-video-intel nvidia nvidia-prime dunst feh
+sudo pacman --noconfirm --needed xorg-server xorg-xinit xsecurelock xss-lock xf86-video-intel nvidia nvidia-prime dunst feh zathura zathura-pdf-poppler
 
 sudo pacman --noconfirm --needed lxappearance pavucontrol
 
