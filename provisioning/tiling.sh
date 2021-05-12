b0173788d044f2dd9d7bbc536d804d4af825b614
Samir Ettali <ettali.samir@gmail.com>
2021-05-12 23:55:45 +0200

    [PROVISIONING] Added pulsemixer

--- provisioning/tiling.sh
+++ provisioning/tiling.sh
@@ -2,7 +2,7 @@
 
 sudo pacman --noconfirm --needed xorg-server xorg-xinit xsecurelock xss-lock xf86-video-intel nvidia nvidia-prime dunst feh zathura zathura-pdf-poppler
 
-sudo pacman --noconfirm --needed lxappearance pavucontrol
+sudo pacman --noconfirm --needed lxappearance pavucontrol pulsemixer
 
 install xorg-xev xorg-xprop xorg-xinput xorg-xbacklight
 
