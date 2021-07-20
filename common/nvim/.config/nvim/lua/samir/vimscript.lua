0a55a304caa6c5e245e8ac101fb28c959729a854
Samir Ettali <ettali.samir@gmail.com>
2021-07-20 22:18:11 +0200

    [NEOVIM] Decompile .class files

--- common/nvim/.config/nvim/lua/samir/vimscript.lua
+++ common/nvim/.config/nvim/lua/samir/vimscript.lua
@@ -25,3 +25,14 @@ vim.api.nvim_exec([[
   inoremap <silent><expr> <Tab>     pumvisible() ? "\<C-n>" : "\<Tab>"
   inoremap <silent><expr> <S-Tab>   pumvisible() ? "\<C-p>" : "\<S-Tab>"
 ]], false)
+
+vim.api.nvim_exec([[
+  augr class
+  au!
+  au bufreadpost,filereadpost *.class %!/usr/bin/jad -noctor -ff -i -p %
+  au bufreadpost,filereadpost *.class set readonly
+  au bufreadpost,filereadpost *.class set ft=java
+  au bufreadpost,filereadpost *.class normal gg=G
+  au bufreadpost,filereadpost *.class set nomodified
+  augr END
+]], false)
