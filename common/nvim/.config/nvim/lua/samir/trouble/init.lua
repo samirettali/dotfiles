497bfaa9cf8130acf61d906971c2655016da1293
Samir Ettali <ettali.samir@gmail.com>
2021-08-11 10:52:30 +0200

    [NEOVIM] Added trouble config

---
+++ common/nvim/.config/nvim/lua/samir/trouble/init.lua
@@ -0,0 +1,8 @@
+vim.cmd[[
+	nnoremap <leader>xx <cmd>TroubleToggle<cr>
+nnoremap <leader>xw <cmd>TroubleToggle lsp_workspace_diagnostics<cr>
+nnoremap <leader>xd <cmd>TroubleToggle lsp_document_diagnostics<cr>
+nnoremap <leader>xq <cmd>TroubleToggle quickfix<cr>
+nnoremap <leader>xl <cmd>TroubleToggle loclist<cr>
+nnoremap <leader>xr <cmd>TroubleToggle lsp_references<cr>
+]]
