644d5964593fbabbe2c1d11d9ac6fc1b248fb306
Samir Ettali <s.ettali@young.business>
2023-05-07 18:53:28 +0200

    wezterm config

--- common/wezterm/.config/wezterm/wezterm.lua
+++ common/wezterm/.config/wezterm/wezterm.lua
@@ -2,7 +2,7 @@ local wezterm = require("wezterm")
 
 local M = {}
 
-M.color_scheme = "Wez"
+M.color_scheme = "Builtin Dark"
 M.font_size = 15
 M.adjust_window_size_when_changing_font_size = false
 
@@ -15,7 +15,7 @@ M.window_padding = {
     bottom = 0,
 }
 
-M.font = wezterm.font("JetBrainsMono Nerd Font Mono", { weight = "Medium", italic = false })
+M.font = wezterm.font("JetBrainsMono Nerd Font Mono", { weight = "Regular", italic = false })
 
 M.custom_block_glyphs = false
 M.harfbuzz_features = { "calt=0", "clig=0", "liga=0" }
