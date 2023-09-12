9499cbb618d426a46a5f705a3fdbfbaab22a9ae6
Samir Ettali <ettali.samir@gmail.com>
2023-09-12 23:11:45 +0200

    dotfiles.nix

---
+++ dotfiles.nix
@@ -0,0 +1,17 @@
+{ ... }: {
+  home.file = {
+    ".config/wezterm/wezterm.lua".source = dotfiles/wezterm.lua;
+    ".Xresources".source = dotfiles/Xresources;
+    ".ackrc".source = dotfiles/ackrc;
+    ".config/alacritty/alacritty.yml".source = dotfiles/alacritty.yml;
+    ".ideavimrc".source = dotfiles/ideavimrc;
+    ".config/foot/foot.ini".source = dotfiles/foot.ini;
+    ".tmux.conf".source = dotfiles/tmux.conf;
+    ".ripgreprc".source = dotfiles/ripgreprc;
+    ".config/kanshi/config".source = dotfiles/kanshi_config;
+    ".config/bc".source = dotfiles/bc;
+    ".config/mpv" = { source = dotfiles/mpv; recursive = true; };
+    ".config/nvim" = { source = dotfiles/nvim; recursive = true; };
+    ".bin" = { source = dotfiles/scripts; recursive = true; };
+  };
+}
