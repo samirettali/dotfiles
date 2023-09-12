cf82fb53d65aace9afd542c80659fcf1f7b90046
Samir Ettali <ettali.samir@gmail.com>
2023-09-12 21:11:37 +0200

    add go bin to path

--- user/zsh.nix
+++ user/zsh.nix
@@ -165,7 +165,7 @@ function () {
   autoload -U edit-command-line
 zle -N edit-command-line
 bindkey '^x^x' edit-command-line
-export PATH=$PATH:~/.bin
+export PATH=$PATH:~/.bin:~/go/bin
         '';
         # promptInit = "autoload -U promptinit && promptinit && prompt suse && setopt prompt_sp";
         shellAliases = {
