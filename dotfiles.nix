b44b7f39f6b920f38c9ea6619d7e36291c551e7c
Samir Ettali <ettali.samir@gmail.com>
2023-09-14 21:32:51 +0200

    remove ackrc

--- dotfiles.nix
+++ dotfiles.nix
@@ -2,7 +2,6 @@
   home.file = {
     ".config/wezterm/wezterm.lua".source = dotfiles/wezterm.lua;
     ".Xdefaults".source = dotfiles/Xdefaults;
-    ".ackrc".source = dotfiles/ackrc;
     ".config/alacritty/alacritty.yml".source = dotfiles/alacritty.yml;
     ".ideavimrc".source = dotfiles/ideavimrc;
     ".config/foot/foot.ini".source = dotfiles/foot.ini;
