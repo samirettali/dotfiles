da57544987384bc1d7e07572950e79fa79763c1f
Samir Ettali <ettali.samir@gmail.com>
2023-09-25 23:21:51 +0200

    chore(neovim): change harpoon shortcut

--- dotfiles/nvim/lua/plugins/configs/harpoon.lua
+++ dotfiles/nvim/lua/plugins/configs/harpoon.lua
@@ -5,7 +5,7 @@ return {
         local ui = require("harpoon.ui")
 
         vim.keymap.set("n", "<Leader>a", mark.add_file)
-        vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu)
+        vim.keymap.set("n", "<Leader>e", ui.toggle_quick_menu)
 
         vim.keymap.set("n", "<Leader>h", function() ui.nav_file(1) end)
         vim.keymap.set("n", "<Leader>j", function() ui.nav_file(2) end)
