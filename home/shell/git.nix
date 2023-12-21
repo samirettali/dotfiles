092c8334a27859c4e37afea6c906351f4ba6a245
Samir Ettali <ettali.samir@gmail.com>
2023-12-21 21:25:22 +0100

    chore: use git instead of gitFull

--- home/shell/git.nix
+++ home/shell/git.nix
@@ -1,25 +1,27 @@
-{ pkgs, ...} : {
-  programs = {
-    git = {
-      enable = true;
-      userName = "Samir Ettali";
-      userEmail = "ettali.samir@gmail.com";
-      package = pkgs.gitFull;
-      extraConfig = {
-        init.defaultBranch = "main";
-        push.autoSetupRemote = true;
-        pull.rebase = false;
-         "url \"git@github.com:\"".insteadOf = "https://github.com/";
-      };
-      ignores = [
-          "*.env"
-          ".env.*"
-          ".DS_Store"
-          "node_modules"
-          "/target"
-          ".idea"
-          "Session.vim"
-      ];
+{ config
+, pkgs
+, ...
+}: {
+  # TODO: use config for name and email
+  programs.git = {
+    enable = true;
+    userName = "Samir Ettali";
+    userEmail = "ettali.samir@gmail.com";
+    extraConfig = {
+      core.editor = config.home.sessionVariables.EDITOR;
+      init.defaultBranch = "main";
+      push.autoSetupRemote = true;
+      pull.rebase = false;
+      url."git@github.com:".insteadOf = "https://github.com/";
     };
+    ignores = [
+      "*.env"
+      ".env*"
+      ".DS_Store"
+      "node_modules"
+      "/target"
+      ".idea"
+      "Session.vim"
+    ];
   };
 }
