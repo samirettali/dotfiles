a80cf14ff016bb7b19fad32f1f2e78aa7d4f505b
Samir Ettali <ettali.samir@gmail.com>
2024-01-04 20:49:09 +0100

    chore(wezterm): add options

--- home/packages/desktop/wezterm.nix
+++ home/packages/desktop/wezterm.nix
@@ -3,12 +3,17 @@
     wezterm = {
       enable = true;
       extraConfig = ''
+        local wezterm = require "wezterm"
         local M = {}
+
+        if wezterm.config_builder then
+          M = wezterm.config_builder()
+        end
 
         M.color_scheme = "Builtin Dark"
         M.font_size = 15
         M.adjust_window_size_when_changing_font_size = false
-        M.enable_wayland = false
+        M.enable_wayland = true
         M.enable_tab_bar = false
 
         M.window_padding = {
@@ -22,6 +27,7 @@
 
         M.custom_block_glyphs = false
         M.harfbuzz_features = { "calt=0", "clig=0", "liga=0" }
+        M.freetype_load_target = "HorizontalLcd"
 
         M.bold_brightens_ansi_colors = true
 
