b0c354204df8cf89c8b0d1d2e7914b767e991fc6
Samir Ettali <ettali.samir@gmail.com>
2024-01-15 21:34:18 +0100

    feat(neovim): add nvim-autopairs plugin

--- home/dotfiles/nvim/lua/plugins/init.lua
+++ home/dotfiles/nvim/lua/plugins/init.lua
@@ -66,6 +66,24 @@ local plugins = {
             require('treesj').setup()
         end,
     },
+    {
+        -- Autopair brackets and other symbols
+        "windwp/nvim-autopairs",
+        dependencies = "hrsh7th/nvim-cmp",
+        config = function()
+            local autopairs = require("nvim-autopairs")
+            local cmp = require("cmp")
+
+            autopairs.setup {
+                fast_wrap = {},
+                disable_filetype = { "TelescopePrompt", "vim" },
+            }
+
+            local cmp_autopairs = require "nvim-autopairs.completion.cmp"
+
+            cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
+        end
+    },
 }
 
 require("lazy").setup {
