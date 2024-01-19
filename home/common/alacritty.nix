2ece6085276033d25501dc90550e9aeca4d6b5ea
Samir Ettali <ettali.samir@gmail.com>
2024-01-19 10:11:19 +0100

    fix(alacritty): use new setting for bright colors

--- home/common/alacritty.nix
+++ home/common/alacritty.nix
@@ -7,9 +7,8 @@
         size = 12;
       };
 
-      draw_bold_text_with_bright_colors = true;
-
       colors = {
+        draw_bold_text_with_bright_colors = true;
         bright = {
           black = "#555555";
           blue = "#5555ff";
