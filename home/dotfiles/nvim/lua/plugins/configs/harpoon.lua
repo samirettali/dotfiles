9cb5fc89f6e0009b34b94fc02255ca3a292a110f
Samir Ettali <ettali.samir@gmail.com>
2024-01-20 18:55:09 +0100

    fix(nvim): missing comma in harpoon config

--- home/dotfiles/nvim/lua/plugins/configs/harpoon.lua
+++ home/dotfiles/nvim/lua/plugins/configs/harpoon.lua
@@ -1,7 +1,7 @@
 return {
     "ThePrimeagen/harpoon",
     branch = "harpoon2",
-    dependencies = { "nvim-lua/plenary.nvim" }
+    dependencies = { "nvim-lua/plenary.nvim" },
     config = function()
         local harpoon = require("harpoon")
 
