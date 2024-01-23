400d66297c4a0be8dd7c25d3df234c759fc34170
Samir Ettali <ettali.samir@gmail.com>
2024-01-23 20:07:14 +0100

    fix(zsh): set shlvl for alacritty

--- home/shell/zsh.nix
+++ home/shell/zsh.nix
@@ -62,7 +62,7 @@
           if [[ "$TERM_PROGRAM" == 'vscode' ]]; then
             local LVL=$(($SHLVL - 3))
           elif [[ -n "$TMUX" ]]; then
-            local LVL=$(($SHLVL - 3))
+            local LVL=$(($SHLVL - 2))
           else
             # Simple terminal
             local LVL=$SHLVL-2
