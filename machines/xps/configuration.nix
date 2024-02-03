0f75f03f138f89ccfec52d208460ae090d2ec0cd
Samir Ettali <ettali.samir@gmail.com>
2024-02-03 16:29:22 +0100

    feat(nixos): generate list of installed packages in /etc/current-system-packages

--- machines/xps/configuration.nix
+++ machines/xps/configuration.nix
@@ -133,6 +133,13 @@
     ];
     etc = {
       "dual-function-keys.yaml".text = builtins.readFile ./dual-function-keys.yaml;
+      "current-system-packages".text =
+        let
+          packages = builtins.map (p: "${p.name}") config.environment.systemPackages;
+          sortedUnique = builtins.sort builtins.lessThan (lib.unique packages);
+          formatted = builtins.concatStringsSep "\n" sortedUnique;
+        in
+          formatted;
     };
   };
 
