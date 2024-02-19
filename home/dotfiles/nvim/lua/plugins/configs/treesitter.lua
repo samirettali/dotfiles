921ccade0bbebc97549a40ac38b998f24551af66
Samir Ettali <ettali.samir@gmail.com>
2024-02-19 20:08:31 +0100

    fix(nvim): run TSUpdate after nvim-treesitter update

--- home/dotfiles/nvim/lua/plugins/configs/treesitter.lua
+++ home/dotfiles/nvim/lua/plugins/configs/treesitter.lua
@@ -1,7 +1,7 @@
 return {
     "nvim-treesitter/nvim-treesitter-textobjects",
     dependencies = "nvim-treesitter/nvim-treesitter",
-    run = ":TSUpdate",
+    build = ":TSUpdate",
     config = function()
         local treesitter = require('nvim-treesitter.configs')
         local options = {
