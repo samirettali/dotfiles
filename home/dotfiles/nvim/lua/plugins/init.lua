c6f610fa5e02cff0684d95a52cf361b3c3c557c1
Samir Ettali <ettali.samir@gmail.com>
2024-02-22 09:34:00 +0100

    chore(nvim): disabled vim-eunuch

--- home/dotfiles/nvim/lua/plugins/init.lua
+++ home/dotfiles/nvim/lua/plugins/init.lua
@@ -2,7 +2,7 @@ local plugins = {
     "tpope/vim-repeat",                           -- Repeat plugin mappings with .
     "tommcdo/vim-exchange",                       -- Exchange two objects
     "sindrets/diffview.nvim",                     -- Improve diff view
-    "tpope/vim-eunuch",                           -- UNIX commands inside neovim
+    -- "tpope/vim-eunuch",                           -- UNIX commands inside neovim, disabled because it broke something
     "rhysd/committia.vim",                        -- Better commit editing
     "tpope/vim-surround",                         -- Add surround object for editing
     "github/copilot.vim",                         -- Copilot
