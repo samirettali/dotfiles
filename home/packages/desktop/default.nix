2f6dc8b17c97dfa14f07b36fd732d8953a1eaf51
Samir Ettali <ettali.samir@gmail.com>
2024-05-13 21:01:54 +0200

    feat(pkgs): add wezterm

--- home/packages/desktop/default.nix
+++ home/packages/desktop/default.nix
@@ -11,4 +11,24 @@
     spotify
     discord
   ];
+
+  programs = {
+    wezterm = {
+      enable = true;
+      extraConfig = ''
+         return {
+           window_padding = {
+             left = 0,
+             right = 0,
+             top = 0,
+             bottom = 0,
+           },
+           font = wezterm.font("JetBrains Mono"),
+           font_size = 16.5,
+           enable_tab_bar = false;
+           color_scheme = "Builtin Dark";
+        }
+      '';
+    };
+  };
 }
