c1e4ec617d386bf7b9e7fde41a63956f0935fbb7
Samir Ettali <ettali.samir@gmail.com>
2024-08-12 23:33:27 +0200

    feat(nvim): add gitsigns shortcuts

--- home/dotfiles/nvim/lua/plugins/git.lua
+++ home/dotfiles/nvim/lua/plugins/git.lua
@@ -4,7 +4,41 @@ return {
     {
         -- Git signs integration
         "lewis6991/gitsigns.nvim",
-        config = true,
+        config = function()
+            local gitsigns = require("gitsigns")
+
+            gitsigns.setup()
+
+            vim.keymap.set('n', ']c', function()
+                if vim.wo.diff then
+                    vim.cmd.normal({ ']c', bang = true })
+                else
+                    gitsigns.nav_hunk('next')
+                end
+            end)
+
+            vim.keymap.set('n', '[c', function()
+                if vim.wo.diff then
+                    vim.cmd.normal({ '[c', bang = true })
+                else
+                    gitsigns.nav_hunk('prev')
+                end
+            end)
+
+            vim.keymap.set("n", "<leader>hr", gitsigns.reset_hunk)
+            vim.keymap.set("n", "<leader>hs", gitsigns.stage_hunk)
+            vim.keymap.set("v", "<leader>hr", function() gitsigns.reset_hunk({ vim.fn.line("."), vim.fn.line("v") }) end)
+            vim.keymap.set("v", "<leader>hs", function() gitsigns.stage_hunk({ vim.fn.line("."), vim.fn.line("v") }) end)
+
+            vim.keymap.set("n", "<leader>hu", gitsigns.undo_stage_hunk)
+            vim.keymap.set("n", "<leader>hp", gitsigns.preview_hunk)
+            vim.keymap.set("n", "<enter>", gitsigns.preview_hunk)
+            vim.keymap.set("n", "<leader>tb", gitsigns.toggle_current_line_blame)
+            vim.keymap.set("n", "<leader>hd", gitsigns.diffthis)
+            vim.keymap.set("n", "<leader>td", gitsigns.toggle_deleted)
+
+            vim.keymap.set({ 'o', 'x' }, 'ih', ':<C-U>Gitsigns select_hunk<CR>')
+        end,
     },
     {
         "rhysd/git-messenger.vim",
