a924d2f0af929fdeb8a3810693874885097e9589
Samir Ettali <ettali.samir@gmail.com>
2024-08-12 04:19:49 +0200

    fix(nvim): use new trouble keymap

--- home/dotfiles/nvim/lua/plugins/trouble.lua
+++ home/dotfiles/nvim/lua/plugins/trouble.lua
@@ -2,7 +2,9 @@ return {
     "folke/trouble.nvim",
     config = function()
         local trouble = require("trouble")
+
         trouble.setup()
-        vim.keymap.set("n", "<leader>tt", function() trouble.toggle() end)
+
+        vim.keymap.set("n", "<leader>td", function() trouble.toggle("diagnostics") end)
     end,
 }
