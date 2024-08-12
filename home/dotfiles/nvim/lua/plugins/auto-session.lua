fe695688fc165d20a866cd8a614f4f2bc8819e8e
Samir Ettali <ettali.samir@gmail.com>
2024-08-12 04:22:04 +0200

    chore(nvim): disable auto session restore

--- home/dotfiles/nvim/lua/plugins/auto-session.lua
+++ home/dotfiles/nvim/lua/plugins/auto-session.lua
@@ -1,17 +1,14 @@
 return {
-    {
-        'rmagatti/auto-session',
-        dependencies = {
-            'nvim-telescope/telescope.nvim',
-        },
-        config = function()
-            local config = {
-                auto_session_enable_last_session = true,
-                auto_save_enabled = true,
-                auto_restore_enabled = true,
-            }
-
-            require('auto-session').setup(config)
-        end,
+    'rmagatti/auto-session',
+    dependencies = {
+        'nvim-telescope/telescope.nvim',
     },
+    config = function()
+        local config = {
+            auto_save_enabled = true,
+            auto_restore_enabled = true,
+        }
+
+        require('auto-session').setup(config)
+    end,
 }
