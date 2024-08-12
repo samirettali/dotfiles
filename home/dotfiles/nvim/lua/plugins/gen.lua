b919965d3756dde331237e5c970bb5fe743de434
Samir Ettali <ettali.samir@gmail.com>
2024-08-12 04:19:11 +0200

    chore(nvim): use llama 3.1 in gen.nvim

--- home/dotfiles/nvim/lua/plugins/gen.lua
+++ home/dotfiles/nvim/lua/plugins/gen.lua
@@ -3,7 +3,7 @@ return {
     "David-Kunz/gen.nvim",
     opts = {
         show_prompt = true,
-        model = "llama3",
+        model = "llama3.1",
         display_mode = "split",
         command = "curl --silent --no-buffer -X POST http://localhost:11434/api/generate -d $body",
     }
