3cc91690c07b577481e8730f444c5b0f027c5d5e
Samir Ettali <ettali.samir@gmail.com>
2024-08-14 20:13:38 +0200

    fix(nvim): update winbar in all windows if the same buffer is shown multiple times

--- home/dotfiles/nvim/lua/core/winbar.lua
+++ home/dotfiles/nvim/lua/core/winbar.lua
@@ -1,7 +1,5 @@
 local utils = require("core.utils")
 
--- "%=",
-
 local function get_lsp_position()
     local ok, navic = pcall(require, "nvim-navic")
 
@@ -16,35 +14,45 @@ local function get_lsp_position()
     return navic.get_location()
 end
 
--- { "CursorMoved", "BufWinEnter", "BufFilePost", "InsertEnter", "BufWritePost" }
+-- TODO: check if BufFilePost and InsertEnter are needed
+-- TODO: maybe we can optimize it by return early if winbar is not changed
 vim.api.nvim_create_autocmd({ "BufWinEnter", "CursorMoved", "CursorMovedI", "BufWritePost" }, {
     callback = function()
-        if utils.is_plugin_filetype() then
-            vim.opt_local.winbar = nil
-            return
-        end
-
-        local parts = {}
-
-        local filename = vim.fn.expand("%:~:.")
-
-        if not utils.is_empty(filename) then
-            table.insert(parts, "")
-
-            if vim.bo.modified then
-                table.insert(parts, "")
+        local function update_winbar()
+            if utils.is_plugin_filetype() then
+                vim.opt_local.winbar = nil
+                return
             end
 
-            table.insert(parts, filename)
+            local parts = {}
+
+            local filename = vim.fn.expand("%:~:.")
+
+            if not utils.is_empty(filename) then
+                table.insert(parts, "")
+
+                if vim.bo.modified then
+                    table.insert(parts, "")
+                end
+
+                table.insert(parts, filename)
+            end
+
+            local lsp = get_lsp_position()
+
+            if not utils.is_empty(lsp) then
+                table.insert(parts, ">")
+                table.insert(parts, lsp)
+            end
+
+            return table.concat(parts, " ")
         end
 
-        local lsp = get_lsp_position()
+        local winbar = update_winbar()
 
-        if not utils.is_empty(lsp) then
-            table.insert(parts, ">")
-            table.insert(parts, lsp)
+        -- Update winbar for all windows displaying the current buffer
+        for _, win in ipairs(vim.fn.getbufinfo(vim.fn.bufnr())[1].windows) do
+            vim.wo[win].winbar = winbar
         end
-
-        vim.opt_local.winbar = table.concat(parts, " ")
     end,
 })
