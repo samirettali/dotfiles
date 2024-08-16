f383550256ed7e0a47fbd4acbb30add2cef78bbf
Samir Ettali <ettali.samir@gmail.com>
2024-08-16 04:12:12 +0200

    refactor(flake): split mac and linux scripts

--- home/dotfiles.nix
+++ home/dotfiles.nix
@@ -1,11 +1,23 @@
-{ ... }: {
-  home.file = {
+{ lib, pkgs, ... }: {
+  home.file = lib.mkMerge [{
     ".ideavimrc".source = dotfiles/ideavimrc;
     ".config/nvim" = { source = dotfiles/nvim; recursive = true; };
-    ".bin" = { source = dotfiles/scripts; recursive = true; };
-    ".config/karabiner" = { source = dotfiles/karabiner; recursive = true; };
+    ".config/karabiner" = lib.mkIf pkgs.stdenv.isDarwin { source = dotfiles/karabiner; recursive = true; };
     ".Xdefaults".source = dotfiles/Xdefaults;
     ".config/zed/settings.json".source = dotfiles/zed.json;
     "revive.toml".source = dotfiles/revive.toml;
-  };
+
+    ".bin/extract".source = dotfiles/scripts/extract.sh;
+    ".bin/glc".source = dotfiles/scripts/glc.sh;
+    ".bin/gr".source = dotfiles/scripts/gr.sh;
+    ".bin/tad".source = dotfiles/scripts/tad.sh;
+  }
+  (lib.mkIf pkgs.stdenv.isLinux {
+    ".bin/passbemenu".source = dotfiles/scripts/passbemenu.sh;
+    ".bin/scratchpad".source = dotfiles/scripts/scratchpad.sh;
+    ".bin/screenshot".source = dotfiles/scripts/screenshot.sh;
+  })
+  (lib.mkIf pkgs.stdenv.isDarwin {
+    ".bin/display".source = dotfiles/scripts/display.sh;
+  })];
 }
