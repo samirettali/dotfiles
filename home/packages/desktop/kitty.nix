d0fef09b7bb3938d3d7ee9de906530925fd01868
Samir Ettali <ettali.samir@gmail.com>
2024-08-16 02:46:14 +0200

    feat(kitty): set repaint_delay, sync_to_monitor and wayland_enable_ime options

--- home/packages/desktop/kitty.nix
+++ home/packages/desktop/kitty.nix
@@ -17,8 +17,11 @@
       map ctrl+shift+plus change_font_size all +0.5
       map ctrl+shift+minus change_font_size all -0.5
       confirm_os_window_close 0
+      repaint_delay 2
       input_delay 0
+      sync_to_monitor no
       disable_ligatures always
+      wayland_enable_ime no
     '';
   };
 }
