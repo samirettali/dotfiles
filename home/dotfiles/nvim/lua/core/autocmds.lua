b045858d34905717562c2ee2906a10447f807334
Samir Ettali <ettali.samir@gmail.com>
2024-08-18 04:59:42 +0200

    fix(nvim): correct usage of highlight on yank

--- home/dotfiles/nvim/lua/core/autocmds.lua
+++ home/dotfiles/nvim/lua/core/autocmds.lua
@@ -9,7 +9,12 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 	desc = "Highlight yanked text",
 	pattern = "*",
 	group = highlight_group,
-	callback = vim.highlight.on_yank,
+	callback = function()
+		vim.highlight.on_yank({
+			higroup = "IncSearch",
+			timeout = 40,
+		})
+	end,
 })
 
 -- vim.api.nvim_create_autocmd("FileType", {
