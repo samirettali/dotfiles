2a614617513124048ac11aa0065080f94ce57b0c
Samir Ettali <ettali.samir@gmail.com>
2024-08-18 05:05:58 +0200

    refactor(nvim): cleanup moonfly colorscheme configuration

--- home/dotfiles/nvim/lua/plugins/moonfly.lua
+++ home/dotfiles/nvim/lua/plugins/moonfly.lua
@@ -3,70 +3,16 @@ return {
 	priority = 1000,
 	config = function()
 		vim.g.moonflyWinSeparator = 2
-		vim.g.moonflyCursorColor = true
 		vim.g.moonflyItalics = false
-		vim.g.moonflyUnderlineMatchParen = true
-		vim.g.moonflyNormalFloat = true
 
 		local moonfly_highlights = vim.api.nvim_create_augroup("MoonflyHighlight", {})
 		vim.api.nvim_create_autocmd("ColorScheme", {
 			pattern = "moonfly",
 			callback = function()
 				local palette = require("moonfly").palette
-				local default_status_bg = palette.grey234
-				local default_status_fg = palette.white
-
 				vim.api.nvim_set_hl(0, "WinBar", { bg = palette.black, fg = palette.grey247 })
 				vim.api.nvim_set_hl(0, "WinBarNC", { bg = palette.black, fg = palette.grey247 })
-				vim.api.nvim_set_hl(0, "CursorLineNr", { bg = palette.black, fg = palette.blue })
-				-- vim.api.nvim_set_hl(0, "FoldColumn", { bg = palette.black, fg = palette.grey241 })
-				-- vim.api.nvim_set_hl(0, "BiscuitColor", { bg = palette.grey234, fg = palette.grey241 })
-				-- vim.api.nvim_set_hl(0, "TreesitterContext", { bg = palette.grey234 })
-				vim.api.nvim_set_hl(0, "Statusline", {
-					bg = palette.black,
-					fg = palette.grey236,
-				})
-				vim.api.nvim_set_hl(0, "StatuslineNC", {
-					bg = palette.black,
-					fg = palette.grey236,
-				})
-				vim.api.nvim_set_hl(0, "DiagnosticSignError", {
-					bg = default_status_bg,
-					fg = palette.red,
-				})
-				vim.api.nvim_set_hl(0, "DiagnosticSignWarn", {
-					bg = default_status_bg,
-					fg = palette.yellow,
-				})
-				vim.api.nvim_set_hl(0, "DiagnosticSignHint", {
-					bg = default_status_bg,
-					fg = palette.purple,
-				})
-				vim.api.nvim_set_hl(0, "DiagnosticSignInfo", {
-					bg = default_status_bg,
-					fg = palette.sky,
-				})
-
-				vim.api.nvim_set_hl(0, "DiagnosticVirtualTextError", {
-					bg = palette.black,
-					fg = palette.red,
-				})
-				vim.api.nvim_set_hl(0, "DiagnosticVirtualTextWarn", {
-					bg = palette.black,
-					fg = palette.yellow,
-				})
-				vim.api.nvim_set_hl(0, "DiagnosticVirtualTextInfo", {
-					bg = palette.black,
-					fg = palette.sky,
-				})
-				vim.api.nvim_set_hl(0, "DiagnosticVirtualTextHint", {
-					bg = palette.black,
-					fg = palette.white,
-				})
-				vim.api.nvim_set_hl(0, "DiagnosticVirtualTextOk", {
-					bg = palette.black,
-					fg = palette.emerald,
-				})
+				vim.api.nvim_set_hl(0, "CursorLineNr", { bg = palette.black })
 			end,
 			group = moonfly_highlights,
 		})
