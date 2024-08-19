0d36d9756707aea745e32b9a2fa46a676ca84941
Samir Ettali <ettali.samir@gmail.com>
2024-08-19 02:18:33 +0200

    refactor(nvim): define augroup only where needed

--- home/dotfiles/nvim/lua/plugins/moonfly.lua
+++ home/dotfiles/nvim/lua/plugins/moonfly.lua
@@ -5,7 +5,6 @@ return {
 		vim.g.moonflyWinSeparator = 2
 		vim.g.moonflyItalics = false
 
-		local moonfly_highlights = vim.api.nvim_create_augroup("MoonflyHighlight", {})
 		vim.api.nvim_create_autocmd("ColorScheme", {
 			pattern = "moonfly",
 			callback = function()
@@ -13,8 +12,11 @@ return {
 				vim.api.nvim_set_hl(0, "WinBar", { bg = palette.black, fg = palette.grey247 })
 				vim.api.nvim_set_hl(0, "WinBarNC", { bg = palette.black, fg = palette.grey247 })
 				vim.api.nvim_set_hl(0, "CursorLineNr", { bg = palette.black })
+				vim.api.nvim_set_hl(0, "TroubleNormal", { bg = palette.black })
+				vim.api.nvim_set_hl(0, "TroubleNormalNC", { bg = palette.black })
+				vim.api.nvim_set_hl(0, "FoldColumn", { bg = palette.black, fg = palette.grey241 })
 			end,
-			group = moonfly_highlights,
+			group = vim.api.nvim_create_augroup("MoonflyHighlight", {}),
 		})
 
 		vim.cmd.colorscheme("moonfly")
