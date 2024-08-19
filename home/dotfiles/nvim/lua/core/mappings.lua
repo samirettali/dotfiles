8bdde6510b72e6efda9f4195a30a18edb246c715
Samir Ettali <ettali.samir@gmail.com>
2024-08-19 18:24:06 +0200

    refactor(nvim): cleanup mappings

--- home/dotfiles/nvim/lua/core/mappings.lua
+++ home/dotfiles/nvim/lua/core/mappings.lua
@@ -1,80 +1,46 @@
 vim.g.mapleader = " "
 
--- Keep text selected after indentating it
-vim.keymap.set("v", "<", "<gv")
-vim.keymap.set("v", ">", ">gv")
-
--- Select last changed text
-vim.keymap.set("n", "gV", "`[v`]")
+vim.keymap.set("n", "<Tab>", "<C-6>", { remap = true })
 
 vim.keymap.set("n", "<Leader>d", "yap<S-}>p", { desc = "Duplicate paragraph" })
+vim.keymap.set("x", "<leader>p", [["_dP]], { desc = "Paste replace visual selection without copying it" })
 
+vim.keymap.set("n", "gV", "`[v`]", { desc = "Select last changed text" })
 vim.keymap.set("n", "<Leader>tl", ":set list!<CR>", { desc = "Toggle listchars" })
 
--- Remove trailing whitespace
--- map("n", "<Leader>t", ":%s/\\s\\+$//e<CR>")
-
-vim.keymap.set("v", "<Leader>p", '"_dP"', { remap = false, desc = "Paste replace visual selection without copying it" })
-
--- Paste last yanked text
--- map("n", ",p", '"0p')
--- map("v", ",P", '"0P')
-
-vim.keymap.set("v", "<C-c>", '"+y', { noremap = false, silent = true, desc = "Copy to clipboard" })
-
--- Keep lines centered when going through search results
-vim.keymap.set("n", "n", "nzzzv")
-vim.keymap.set("n", "N", "Nzzzv")
-
--- Keep cursor centered when scrolling
-vim.keymap.set("n", "<C-d>", "<C-d>zz", { noremap = true, silent = true })
-vim.keymap.set("n", "<C-u>", "<C-u>zz", { noremap = true, silent = true })
-
--- Center <C-o> and <C-i>
-vim.keymap.set("n", "<C-o>", "<C-o>zzzv")
-vim.keymap.set("n", "<C-i>", "<C-i>zzzv")
-
--- Mantain cursor position when joining lines
-vim.keymap.set("n", "J", "mzJ`z")
+vim.keymap.set("n", "J", "mzJ`z", { desc = "Keep cursor position when joining lines" })
+vim.keymap.set("n", "n", "nzzzv", { desc = "Center next search result" })
+vim.keymap.set("n", "N", "Nzzzv", { desc = "Center previous search result" })
+vim.keymap.set("n", "<C-o>", "<C-o>zzzv", { desc = "Center previous jump" })
+vim.keymap.set("n", "<C-i>", "<C-i>zzzv", { desc = "Center next jump" })
 
 -- map("n", "<C-j>", ":cnext<CR>zzzv")
 
--- Undo break points
-vim.keymap.set("i", ",", ",<C-g>u")
-vim.keymap.set("i", ".", ".<C-g>u")
-vim.keymap.set("i", "!", "!<C-g>u")
-vim.keymap.set("i", "?", "?<C-g>u")
-vim.keymap.set("i", "[", "[<C-g>u")
-vim.keymap.set("i", "]", "]<C-g>u")
+vim.keymap.set("i", ",", ",<C-g>u", { desc = "Treat , as un undo breakpoint" })
+vim.keymap.set("i", ".", ".<C-g>u", { desc = "Treat . as un undo breakpoint" })
+vim.keymap.set("i", "!", "!<C-g>u", { desc = "Treat ! as un undo breakpoint" })
+vim.keymap.set("i", "?", "?<C-g>u", { desc = "Treat ? as un undo breakpoint" })
+vim.keymap.set("i", "[", "[<C-g>u", { desc = "Treat [ as un undo breakpoint" })
+vim.keymap.set("i", "]", "]<C-g>u", { desc = "Treat ] as un undo breakpoint" })
 
-vim.keymap.set("n", "<C-p>", ":bp<CR>", { silent = true, desc = "Go to previous buffer" })
-vim.keymap.set("n", "<C-n>", ":bn<CR>", { silent = true, desc = "Go to next buffer" })
+vim.keymap.set("n", "<C-p>", ":bp<CR>", { desc = "Go to previous buffer", silent = true })
+vim.keymap.set("n", "<C-n>", ":bn<CR>", { desc = "Go to next buffer", silent = true })
 
-vim.keymap.set("n", "<Tab>", ":tabnext<CR>", { silent = true, desc = "Go to next tab" })
-vim.keymap.set("n", "<S-Tab>", ":tabp<CR>", { silent = true, desc = "Go to previous tab" })
-
--- Move selected lines
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", { noremap = true, silent = true })
-vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", { noremap = true, silent = true })
-
-vim.keymap.set({ "n", "v" }, "<space>", "<nop>", { silent = true })
-
--- Paste replacing selected text but without copying it
-vim.keymap.set("x", "<leader>p", [["_dP]])
-
--- vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
--- vim.keymap.set("n", "<leader>Y", [["+Y]])
-
--- FIXME: comment block and duplicate
-vim.keymap.set("v", "<Leader>D", [[y`>pgv:norm gcc<CR>`>j^]], { silent = true })
-
-vim.keymap.set("n", "<Leader>y", ":%y +<CR>", { noremap = false, silent = true, desc = "Copy entire buffer" })
-
--- zoom current split
-vim.keymap.set("n", "<leader>z", "<C-w>o<C-w>|", { silent = true, desc = "Maximize current split" })
+vim.keymap.set({ "n", "v" }, "<space>", "<nop>", { desc = "Disable space", silent = true })
+vim.keymap.set({ "n", "v" }, "<del>", "<nop>", { desc = "Disable space", silent = true })
 
 -- Resize splits
 vim.keymap.set("n", "<M-,>", "<C-w>5<", { silent = true })
 vim.keymap.set("n", "<M-.>", "<C-w>5>", { silent = true })
 vim.keymap.set("n", "<M-t>", "<C-w>+", { silent = true })
 vim.keymap.set("n", "<M-s>", "<C-w>-", { silent = true })
+
+-- Old stuff
+
+-- Keep text selected after indentating it
+-- vim.keymap.set("v", "<", "<gv")
+-- vim.keymap.set("v", ">", ">gv")
+
+-- Keep cursor centered when scrolling
+-- vim.keymap.set("n", "<C-d>", "<C-d>zz", { noremap = true, silent = true })
+-- vim.keymap.set("n", "<C-u>", "<C-u>zz", { noremap = true, silent = true })
