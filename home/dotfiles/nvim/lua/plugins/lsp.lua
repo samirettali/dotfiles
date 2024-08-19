1cce61aa160513e4b365b096945a1c30f4ad18cc
Samir Ettali <ettali.samir@gmail.com>
2024-08-19 18:29:03 +0200

    feat(nvim): use telescope for lsp mappings

--- home/dotfiles/nvim/lua/plugins/lsp.lua
+++ home/dotfiles/nvim/lua/plugins/lsp.lua
@@ -1,6 +1,8 @@
 local methods = vim.lsp.protocol.Methods
 
 local function custom_implementation_provider(bufnr)
+	-- local telescope = require("telescope.builtin")
+
 	return function()
 		local params = vim.lsp.util.make_position_params()
 
@@ -22,7 +24,9 @@ local function custom_implementation_provider(bufnr)
 				end
 			end
 
+			-- TODO: use telescope
 			vim.lsp.handlers[methods.textDocument_implementation](err, result, ctx, config)
+			-- telescope.lsp_implementations(err, result, ctx, config)
 			vim.cmd([[normal! zz]])
 		end)
 	end
@@ -30,6 +34,7 @@ end
 
 return {
 	dependencies = {
+		"nvim-telescope/telescope.nvim",
 		"stevearc/conform.nvim",
 		"mfussenegger/nvim-lint",
 		"SmiteshP/nvim-navic", -- LSP breadcrumbs
@@ -69,6 +74,8 @@ return {
 					vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled())
 				end
 
+				local telescope = require("telescope.builtin")
+
 				local default_opts = { buffer = args.buf }
 				if client.supports_method(methods.textDocument_inlayHint) then
 					vim.keymap.set("n", "<Leader>ti", toggle_lsp_hints, default_opts)
@@ -79,21 +86,22 @@ return {
 				end
 
 				if client.supports_method(methods.textDocument_definition) then
-					vim.keymap.set("n", "gd", vim.lsp.buf.definition, default_opts)
+					vim.keymap.set("n", "gd", telescope.lsp_definitions, default_opts)
 				end
+
 				if client.supports_method(methods.textDocument_declaration) then
 					vim.keymap.set("n", "gD", vim.lsp.buf.declaration, default_opts)
 				end
 				if client.supports_method(methods.textDocument_typeDefinition) then
-					vim.keymap.set("n", "gT", vim.lsp.buf.type_definition, default_opts)
+					vim.keymap.set("n", "gT", telescope.lsp_type_definitions, default_opts)
 				end
 
 				if client.supports_method(methods.textDocument_documentSymbol) then
-					vim.keymap.set("n", "gs", vim.lsp.buf.document_symbol, default_opts)
+					vim.keymap.set("n", "gs", telescope.lsp_document_symbols, default_opts)
 				end
 
 				if client.supports_method(methods.workspace_symbol) then
-					vim.keymap.set("n", "gS", vim.lsp.buf.workspace_symbol, default_opts)
+					vim.keymap.set("n", "gS", telescope.lsp_dynamic_workspace_symbols, default_opts)
 				end
 
 				if client.supports_method(methods.textDocument_codeAction) then
@@ -104,21 +112,31 @@ return {
 					vim.keymap.set("n", "gr", vim.lsp.buf.rename, default_opts)
 				end
 
+				if client.supports_method(methods.textDocument_references) then
+					vim.keymap.set("n", "gR", telescope.lsp_references)
+				end
+
+				if client.supports_method(methods.textDocument_references) then
+					vim.keymap.set("n", "gR", telescope.lsp_references)
+				end
+
 				if client.supports_method(methods.textDocument_signatureHelp) then
 					vim.keymap.set("i", "<C-h>", vim.lsp.buf.signature_help, default_opts)
 				end
 
 				if client.supports_method(methods.callHierarchy_incomingCalls) then
-					vim.keymap.set("n", "gI", vim.lsp.buf.incoming_calls, default_opts)
+					vim.keymap.set("n", "gI", telescope.lsp_incoming_calls, default_opts)
 				end
 
 				if client.supports_method(methods.callHierarchy_outgoingCalls) then
-					vim.keymap.set("n", "gO", vim.lsp.buf.outgoing_calls, default_opts)
+					vim.keymap.set("n", "gO", telescope.lsp_outgoing_calls, default_opts)
 				end
 
 				if client.supports_method(methods.textDocument_diagnostic) then
-					vim.keymap.set("n", "ds", vim.diagnostic.open_float, default_opts)
+					vim.keymap.set("n", "<leader>gd", telescope.diagnostics, default_opts)
+					-- vim.keymap.set("n", "ds", vim.diagnostic.open_float, default_opts)
 
+					-- TODO: float is not working
 					local next_opts = { count = 1, wrap = true, float = true }
 					vim.keymap.set("n", "]d", function()
 						vim.diagnostic.jump(next_opts)
