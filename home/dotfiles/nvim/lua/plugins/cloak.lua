99f50fbde98b88ea9aa625c93bd5032e90181c33
Samir Ettali <ettali.samir@gmail.com>
2024-08-19 17:57:53 +0200

    fix(nvim): add missing setup function call to cloak.nvim

--- home/dotfiles/nvim/lua/plugins/cloak.lua
+++ home/dotfiles/nvim/lua/plugins/cloak.lua
@@ -1,7 +1,17 @@
 return {
 	-- Hide secrets in .env files
+	-- TODO: add docker-compose.yml and Dockerfile
 	"laytan/cloak.nvim",
 	config = function()
-		vim.keymap.set("n", "<leader>tc", "<cmd>CloakToggle<cr>", { noremap = true, silent = true })
+		require("cloak").setup({
+			patterns = {
+				{
+					file_pattern = { ".env*", "*.env*" },
+					cloak_pattern = "=.+",
+				},
+			},
+		})
+
+		vim.keymap.set("n", "<leader>tc", "<cmd>CloakToggle<cr>", { silent = true })
 	end,
 }
