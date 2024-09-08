5556be7afacff0dc6e57a8eb7e60b1c5a687b05e
Samir Ettali <ettali.samir@gmail.com>
2024-09-09 01:00:00 +0200

    feat(nvim): add go to next and prev diagnostic mapping in trouble.nvim

--- home/dotfiles/nvim/lua/plugins/trouble.lua
+++ home/dotfiles/nvim/lua/plugins/trouble.lua
@@ -5,8 +5,16 @@ return {
 
 		trouble.setup()
 
-		vim.keymap.set("n", "<leader>td", function()
+		vim.keymap.set("n", "<leader>te", function()
 			trouble.toggle("diagnostics")
+		end)
+
+		vim.keymap.set("n", "]e", function()
+			trouble.prev({ skip_groups = true, jump = true, mode = "diagnostics" })
+		end)
+
+		vim.keymap.set("n", "[e", function()
+			trouble.next({ skip_groups = true, jump = true, mode = "diagnostics" })
 		end)
 	end,
 }
