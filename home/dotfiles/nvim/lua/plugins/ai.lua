23b0cc68240f08edb1401d62e1e6115234534fdc
Samir Ettali <ettali.samir@gmail.com>
2024-09-10 00:32:54 +0200

    chore(nvim): restore nvim-web-devicons for avante

--- home/dotfiles/nvim/lua/plugins/ai.lua
+++ home/dotfiles/nvim/lua/plugins/ai.lua
@@ -17,8 +17,7 @@ return {
 		event = "VeryLazy",
 		build = "make",
 		dependencies = {
-			-- "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
-			"echasnovski/mini.icons",
+			"nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
 			"stevearc/dressing.nvim",
 			"nvim-lua/plenary.nvim",
 			"MunifTanjim/nui.nvim",
