e8dabe5f1be146c24a1de4559b715204439909b2
Samir Ettali <ettali.samir@gmail.com>
2024-09-13 22:35:27 +0200

    feat(flake): download ollama models

--- home/default.nix
+++ home/default.nix
@@ -1,4 +1,8 @@
-{pkgs, ...}: {
+{
+  pkgs,
+  lib,
+  ...
+}: {
   imports = [
     ./dotfiles.nix
     ./shell
@@ -17,4 +21,35 @@
     ollama
     # aider-chat
   ];
+
+  home.activation = {
+    downloadOllamaModels = lib.hm.dag.entryAfter ["writeBoundary"] ''
+      models=(
+          "CognitiveComputations/dolphin-gemma2:2b-v2.9.4-Q8_0"
+          "ajindal/llama3.1-storm:8b"
+          "ajindal/llama3.1-storm:8b-Q8_0"
+          "codegemma:latest"
+          "hermes3:8b-llama3.1-fp16"
+          "hermes3:8b"
+          "llama3.1:8b"
+          "llama3.1:8b-instruct-fp16"
+          "mistral-nemo:latest"
+          "mxbai-embed-large:latest"
+          "phi3.5:3.8b"
+          "phi3.5:3.8b-mini-instruct-fp16"
+          "qwen2:7b"
+          "smollm:135m-base-v0.2-fp16"
+          "smollm:135m-instruct-v0.2-q8_0"
+          "tinyllama"
+          "tinyllama:1.1b-chat-v0.6-fp16"
+          "tinyllama:latest"
+          "yi-coder:9b"
+      )
+
+      for model in "''${models[@]}"; do
+          echo "Downloading $model"
+          $DRY_RUN_CMD ${pkgs.ollama}/bin/ollama pull $model
+      done
+    '';
+  };
 }
