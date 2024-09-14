fbf5cd84a6062cc1721edf1129612492099c4835
Samir Ettali <ettali.samir@gmail.com>
2024-09-14 23:59:56 +0200

    feat(nvim): add telescope buffer keymap

--- home/dotfiles/nvim/lua/plugins/telescope.lua
+++ home/dotfiles/nvim/lua/plugins/telescope.lua
@@ -65,6 +65,7 @@ return {
 			-- Telescope
 			vim.keymap.set("n", "<C-f>", builtin.find_files)
 			vim.keymap.set("n", "<C-g>", builtin.live_grep)
+			vim.keymap.set("n", "<leader>b", builtin.buffers)
 
 			vim.keymap.set("n", "<leader>ps", function()
 				builtin.grep_string({ search = vim.fn.input("grep: ") })
