5f5c90936a68e1c6a14b8e9bbcd7397561136bb0
Samir Ettali <ettali.samir@gmail.com>
2024-09-15 00:04:38 +0200

    feat(nvim): add arrow.nvim plugin

---
+++ home/dotfiles/nvim/lua/plugins/arrow.lua
@@ -0,0 +1,8 @@
+return {
+	"otavioschwanck/arrow.nvim",
+	opts = {
+		show_icons = true,
+		leader_key = ";", -- Recommended to be a single key
+		buffer_leader_key = "m", -- Per Buffer Mappings
+	},
+}
