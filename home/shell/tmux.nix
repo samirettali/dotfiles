3adc24a8687e6b465ecf56858c447a1c1e83579a
Samir Ettali <ettali.samir@gmail.com>
2024-10-14 19:37:36 +0200

    fix(tmux): force zsh usage

--- home/shell/tmux.nix
+++ home/shell/tmux.nix
@@ -3,16 +3,23 @@
     aggressiveResize = true;
     enable = true;
     escapeTime = 0;
-    extraConfig = builtins.readFile ../dotfiles/tmux.conf;
+    # Read tmux.conf and concat default shell
+    extraConfig =
+      builtins.readFile ../dotfiles/tmux.conf
+      + ''
+        set-option -g default-shell ${pkgs.zsh}/bin/zsh
+        set-option -g default-command ${pkgs.zsh}/bin/zsh
+      '';
     historyLimit = 100000;
     keyMode = "vi";
     mouse = true;
-    newSession = true;
+    newSession = false;
     plugins = with pkgs.tmuxPlugins; [
       vim-tmux-navigator
       t-smart-tmux-session-manager
     ];
     shortcut = "a";
+    shell = "${pkgs.zsh}/bin/zsh";
   };
 
   home.sessionPath = [
