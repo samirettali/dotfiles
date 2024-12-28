b8f61f4605d5924e3818b660c0f91dc8ce20b503
Samir Ettali <ettali.samir@gmail.com>
2024-12-28 20:07:45 +0100

    chore(nvim): remove symbol-usage.nvim

--- home/dotfiles/nvim/lua/plugins/symbols-usage.lua
+++
@@ -1,9 +0,0 @@
-return {
-	"Wansmer/symbol-usage.nvim",
-	config = function()
-		local usage = require("symbol-usage")
-		usage.setup()
-
-		vim.keymap.set("n", "<leader>tu", usage.toggle_globally)
-	end,
-}
