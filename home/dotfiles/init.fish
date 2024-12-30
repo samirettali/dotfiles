a2157da9e55e42bfa271c69c85be279b03c75c27
Samir Ettali <ettali.samir@gmail.com>
2024-12-30 16:00:54 +0100

    feat(fish): add branch, exit code and branch to right prompt

--- home/dotfiles/init.fish
+++ home/dotfiles/init.fish
@@ -10,28 +10,62 @@ function fish_mode_prompt
 end
 
 function fish_prompt
+    # Select symbol
     set -l symbol '$ '
     if fish_is_root_user
         set symbol '# '
     end
 
-    set_color -o blue
+    # Show user@host if connected via SSH
+    set -l ssh
     if set -q SSH_CLIENT || set -q SSH_TTY
-        printf '%s@%s' $USER (hostname)
-        set_color -o normal
-        printf ':'
-        set_color -o blue
+        set ssh (set_color -o blue) $USER '@' (hostname) (set_color -o normal) ":" (set_color normal)
     end
-    echo -n (prompt_pwd)
 
-    set_color -o red
-    echo -n $symbol
+    set -l pwd (set_color -o blue) (prompt_pwd) (set_color normal)
+    set -l symbol (set_color -o red) $symbol (set_color normal)
 
-    set_color normal
+    printf (string join '' -- $ssh $pwd $symbol)
 end
 
 function fish_right_prompt
-    set_color -o brmagenta
-    printf (fish_git_prompt "[%s]")
-    set_color normal
+    # Show last status code if != 0
+    set -l last_status $status
+
+    set -l stat
+    if test $last_status -ne 0
+        set stat (set_color -o bryellow)"($last_status)"(set_color normal)
+    end
+
+    # Git prompt
+    set -l gp (set_color -o brmagenta)(fish_git_prompt "[%s]")(set_color normal)
+
+
+    # Duration
+    # TODO: maybe show only if > 3 seconds
+    set -l d $CMD_DURATION
+    set -l second 1000
+    set -l minute (math 60 \* $second)
+    set -l hour (math $minute \* 60)
+    set -l s (math -s0 $d / $second)
+    set -l m (math -s0 $d / $minute)
+    set -l h (math -s0 $d / $hour)
+    set -l duration
+
+    if test $h -gt 0
+        set h (math -s2 $d / $hour)
+        set duration $h'h'
+    else if test $m -gt 0
+        set m (math -s2 $d / $minute)
+        set duration $m'm'
+    else if test $s -gt 0
+        set s (math -s2 $d / $second)
+        set duration $s's'
+    else
+        set duration $d'ms'
+    end
+
+    set duration (set_color -id white)$duration(set_color normal)
+
+    printf (string join ' ' -- $duration $stat $gp)
 end
