3f4286723f96c9f464f487911aef92c8fbbad17b
Samir Ettali <s.ettali@young.business>
2025-02-10 23:29:54 +0100

    chore(nvim): set supermaven keymaps

--- home/dotfiles/nvim/lua/plugins/supermaven.lua
+++ home/dotfiles/nvim/lua/plugins/supermaven.lua
@@ -1,8 +1,12 @@
 return {
 	"supermaven-inc/supermaven-nvim",
 	opts = {
-		log_level = "off",
-		accept_suggestion = "<C-f>",
+		log_level = "info",
+		keymaps = {
+			accept_suggestion = "<C-f>",
+			clear_suggestion = "<C-]>",
+			accept_word = "<C-j>",
+		},
 		condition = function()
 			return string.match(vim.fn.expand("%:t"), ".envrc")
 		end,
