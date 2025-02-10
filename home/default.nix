3f5818be62a26d77586363dc10104bddc05d9cf8
Samir Ettali <s.ettali@young.business>
2025-02-10 18:17:40 +0100

    chore(ollama): download llama3.1 and deepseek-r1:8b

--- home/default.nix
+++ home/default.nix
@@ -18,33 +18,15 @@
   };
 
   home.packages = with pkgs; [
+    aider-chat
     ollama
-    # aider-chat
   ];
 
   home.activation = {
     downloadOllamaModels = lib.hm.dag.entryAfter ["writeBoundary"] ''
       models=(
-          "CognitiveComputations/dolphin-gemma2:2b-v2.9.4-Q8_0"
-          "ajindal/llama3.1-storm:8b"
-          "ajindal/llama3.1-storm:8b-Q8_0"
-          "codegemma:latest"
-          "hermes3:8b"
-          "hermes3:8b-llama3.1-fp16"
-          "llama3.1:8b"
-          "llama3.1:8b-instruct-fp16"
-          "mistral-nemo:latest"
-          "mxbai-embed-large:latest"
-          "phi3.5:3.8b"
-          "phi3.5:3.8b-mini-instruct-fp16"
-          "qwen2.5-coder:7b"
-          "qwen2.5:7b"
-          "smollm:135m-base-v0.2-fp16"
-          "smollm:135m-instruct-v0.2-q8_0"
-          "tinyllama"
-          "tinyllama:1.1b-chat-v0.6-fp16"
-          "tinyllama:latest"
-          "yi-coder:9b"
+          deepseek-r1:8b
+          llama3.1
       )
 
       for model in "''${models[@]}"; do
