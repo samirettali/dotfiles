586e255e8f648e66da7e85c9ebf5df2699040536
Samir Ettali <s.ettali@young.business>
2025-03-05 20:01:06 +0100

    fix(sketchybar): use correct directory

--- home/dotfiles/sketchybar/items/spaces.sh
+++ home/dotfiles/sketchybar/items/spaces.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 sketchybar --add event aerospace_workspace_change
+
 for sid in $(aerospace list-workspaces --all); do
     sketchybar --add item space.$sid left \
         --subscribe space.$sid aerospace_workspace_change \
@@ -11,5 +12,5 @@ for sid in $(aerospace list-workspaces --all); do
         background.drawing=off \
         label="$sid" \
         click_script="aerospace workspace $sid" \
-        script="$CONFIG_DIR/plugins/aerospace.sh $sid"
+        script="$PLUGIN_DIR/aerospace.sh $sid"
 done
