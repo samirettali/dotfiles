1609c56d42e7e3d9a118fb27231eb7b8a5f00358
Samir Ettali <s.ettali@young.business>
2025-03-11 00:35:26 +0100

    feat(pkgs): add hakrawler, jadx, ghidra and rlwrap

--- home/packages/security.nix
+++ home/packages/security.nix
@@ -4,14 +4,18 @@
     amass
     cent
     dnsx
+    hakrawler
     ffuf
     findomain
     httpx
+    jadx
     naabu
+    ghidra-bin
     nmap
     nuclei
     nuclei-templates
     sqlmap
     subfinder
+    rlwrap # TODO: move to shell/default.nix
   ];
 }
