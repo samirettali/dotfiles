363f7a422e04e229e530560a7bbc8f09b0795678
Samir Ettali <s.ettali@young.business>
2025-03-12 01:36:50 +0100

    feat(git): set pull to only fast forward only and zdiff3 merge style

--- home/shell/git.nix
+++ home/shell/git.nix
@@ -3,80 +3,74 @@
   pkgs,
   ...
 }: {
-  programs.git = {
-    enable = true;
-    userEmail = config.home.sessionVariables.EMAIL;
-    userName = "Samir Ettali";
-    extraConfig = {
-      branch.sort = "-committerdate";
-      column.ui = "auto";
-      commit.verbose = true;
-      commit.gpgsign = true;
-      tag.gpgsign = true;
-      gpg.format = "ssh";
-      core.editor = config.home.sessionVariables.EDITOR;
-      diff = {
-        algorithm = "histogram";
-        colorMoved = "plain";
-        mnemonicPrefix = true;
-        renames = true;
-      };
-      # TODO: fix existing repos
-      # fetch = {
-      # prune = true;
-      # pruneTags = true;
-      # all = true;
-      # };
-      help.autocorrect = "prompt";
-      init.defaultBranch = "main";
-      pull.rebase = true;
-      push = {
-        autoSetupRemote = true;
-        default = "simple";
-      };
-      tag.sort = "version:refname";
-      url = {
-        "git@github.com:samirettali" = {
-          insteadOf = "https://github.com/samirettali";
-        };
-        "git@github.com:YoungAgency" = {
-          insteadOf = "https://github.com/YoungAgency";
-        };
-      };
-      user.signingkey = "${config.home.homeDirectory}/.ssh/github.pub";
-    };
-    ignores = [
-      "*.env"
-      ".env*"
-      ".DS_Store"
-      "node_modules"
-      "/target"
-      ".idea"
-      "Session.vim"
-    ];
-    signing.format = "ssh";
-
-    # TODO: this is broken in nixpkgs
-    # delta = {
-    #   enable = true;
-    # };
-  };
-
-  programs.lazygit = {
-    enable = true;
-    settings = {
-      git = {
-        paging = {
-          colorArg = "always";
-          # pager = "delta --dark --paging never";
-        };
-      };
-      gui = {
-        showBottomLine = false;
-      };
-    };
-  };
   home.packages = with pkgs; [
     git-absorb
   ];
+
+  programs = {
+    git = {
+      enable = true;
+      userEmail = config.home.sessionVariables.EMAIL;
+      userName = "Samir Ettali";
+      extraConfig = {
+        branch.sort = "-committerdate";
+        column.ui = "auto";
+        commit.gpgsign = true;
+        commit.verbose = true;
+        core.editor = config.home.sessionVariables.EDITOR;
+        diff = {
+          algorithm = "histogram";
+          colorMoved = "plain";
+          mnemonicPrefix = true;
+          renames = true;
+        };
+        gpg.format = "ssh";
+        help.autocorrect = "prompt";
+        init.defaultBranch = "main";
+        merge.confictStyle = "zdiff3";
+        pull.ff = "only";
+        push.autoSetupRemote = true;
+        push.default = "simple";
+        tag.gpgsign = true;
+        tag.sort = "version:refname";
+        url = {
+          "git@github.com:samirettali" = {
+            insteadOf = "https://github.com/samirettali";
+          };
+          "git@github.com:YoungAgency" = {
+            insteadOf = "https://github.com/YoungAgency";
+          };
+        };
+        user.signingkey = "${config.home.homeDirectory}/.ssh/github.pub";
+        # TODO: fix
+        # includeIf."gitdir:~/dev/dotfiles/" = {
+        #   email = "ettali.samir@gmail.com";
+        # };
+        # fetch = {
+        # prune = true;
+        # pruneTags = true;
+        # all = true;
+        # };
+      };
+      ignores = [
+        "*.env"
+        ".env*"
+        ".DS_Store"
+        "node_modules"
+        "/target"
+        ".idea"
+        "Session.vim"
+      ];
+      signing.format = "ssh";
+    };
+
+    lazygit = {
+      enable = true;
+      settings = {
+        gui = {
+          showBottomLine = false;
+        };
+      };
+    };
+  };
 }
