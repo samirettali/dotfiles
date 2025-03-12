f5b7016aefdb5ba28cd4987db3e4c9b7c92ac91c
Samir Ettali <s.ettali@young.business>
2025-03-12 02:44:54 +0100

    feat: set use touch for sudo

--- machines/mbp.nix
+++ machines/mbp.nix
@@ -31,6 +31,9 @@
 
   time.timeZone = "Europe/Rome";
 
+  # TODO: can this be used in the terminal? and maybe move to common config with linux
+  security.pam.services.sudo_local.touchIdAuth = true;
+
   system = {
     keyboard = {
       enableKeyMapping = true;
