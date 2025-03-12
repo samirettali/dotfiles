8fee80d73947db360040be11af207d38086aac19
Samir Ettali <ettali.samir@gmail.com>
2025-03-12 04:40:21 +0100

    refactor(vscode): use new profiles.default configuration

--- home/packages/desktop/vscode.nix
+++ home/packages/desktop/vscode.nix
@@ -6,92 +6,94 @@
   programs = {
     vscode = {
       enable = true;
-      enableExtensionUpdateCheck = false;
-      enableUpdateCheck = false;
       mutableExtensionsDir = false;
-      extensions = with pkgs.vscode-marketplace; [
-        amos402.scope-bar
-        eamodio.gitlens
-        franzgollhammer.jb-fleet-dark
-        github.copilot
-        github.copilot-chat
-        github.vscode-pull-request-github
-        golang.go
-        jnoortheen.nix-ide
-        juanblanco.solidity
-        mkhl.direnv
-        rust-lang.rust-analyzer
-        vscodevim.vim
-        # ms-toolsai.jupyter
-        # marketplace.continue.continue
-        # marketplace.saoudrizwan.claude-dev
-        # ms-python.python
-        # supermaven.supermaven
-      ];
-      keybindings = [];
-      userSettings = {
-        # TODO: enable copilot agent
-        "editor.fontFamily" = customArgs.font.name;
-        "editor.fontSize" = customArgs.font.size;
-        "editor.minimap.enabled" = false;
-        "editor.formatOnSave" = true;
-        "editor.renderWhitespace" = "trailing";
-        "editor.lineNumbers" = "relative";
-        "editor.wordSeparators" = "/\\()\"'=,.;<>~!@#$%^&*|+=[]{}`?-";
-        "editor.inlineSuggest.suppressSuggestions" = true;
-        "editor.inlayHints.enabled" = "offUnlessPressed";
-
-        "diffEditor.ignoreTrimWhitespace" = false;
-
-        "files.exclude" = {
-          "**/.classpath" = true;
-          "**/.project" = true;
-          "**/.settings" = true;
-          "**/.factorypath" = true;
-        };
-
-        "telemetry.telemetryLevel" = "off";
-
-        "workbench.tree.indent" = 24;
-        "workbench.welcomePage.walkthroughs.openOnInstall" = false;
-        "workbench.startupEditor" = "none";
-        "workbench.colorTheme" = "Fleet Dark";
-        "workbench.sideBar.location" = "right";
-
-        "github.copilot.editor.enableAutoCompletions" = true;
-        "github.copilot.nextEditSuggestions.enabled" = true;
-
-        # Go
-        "go.formatTool" = "gofumpt";
-        "go.delveConfig" = {
-          "showGlobalVariables" = true;
-        };
-        "go.lintTool" = "revive";
-        # "go.lintFlags" = [
-        #   "--enable-all"
-        # ];
-        "go.coverOnSingleTest" = true;
-        "go.showWelcome" = true;
-        "gopls" = {
-          "ui.diagnostic.analyses" = {
-            "modernize" = true;
-          };
-          "ui.semanticTokens" = false;
-        };
-
-        "nix.formatterPath" = "alejandra";
-
-        "vim.handleKeys" = {
-          "<C-p>" = false;
-        };
-
-        "solidity.packageDefaultDependenciesContractsDirectory" = "src";
-        "solidity.packageDefaultDependenciesDirectory" = "lib";
-        "solidity.compileUsingRemoteVersion" = "v0.8.23";
-        "solidity.remappings" = [
-          "@openzeppelin/contracts/=lib/openzeppelin-contracts-upgradeable/lib/openzeppelin-contracts/contracts/"
-          "@openzeppelin/contracts-upgradeable/=lib/openzeppelin-contracts-upgradeable/contracts/"
+      profiles.default = {
+        enableExtensionUpdateCheck = false;
+        enableUpdateCheck = false;
+        extensions = with pkgs.vscode-marketplace; [
+          amos402.scope-bar
+          eamodio.gitlens
+          franzgollhammer.jb-fleet-dark
+          github.copilot
+          github.copilot-chat
+          github.vscode-pull-request-github
+          golang.go
+          jnoortheen.nix-ide
+          juanblanco.solidity
+          mkhl.direnv
+          rust-lang.rust-analyzer
+          vscodevim.vim
+          # ms-toolsai.jupyter
+          # marketplace.continue.continue
+          # marketplace.saoudrizwan.claude-dev
+          # ms-python.python
+          # supermaven.supermaven
         ];
+        keybindings = [];
+        userSettings = {
+          # TODO: enable copilot agent
+          "editor.fontFamily" = customArgs.font.name;
+          "editor.fontSize" = customArgs.font.size;
+          "editor.minimap.enabled" = false;
+          "editor.formatOnSave" = true;
+          "editor.renderWhitespace" = "trailing";
+          "editor.lineNumbers" = "relative";
+          "editor.wordSeparators" = "/\\()\"'=,.;<>~!@#$%^&*|+=[]{}`?-";
+          "editor.inlineSuggest.suppressSuggestions" = true;
+          "editor.inlayHints.enabled" = "offUnlessPressed";
+
+          "diffEditor.ignoreTrimWhitespace" = false;
+
+          "files.exclude" = {
+            "**/.classpath" = true;
+            "**/.project" = true;
+            "**/.settings" = true;
+            "**/.factorypath" = true;
+          };
+
+          "telemetry.telemetryLevel" = "off";
+
+          "workbench.tree.indent" = 24;
+          "workbench.welcomePage.walkthroughs.openOnInstall" = false;
+          "workbench.startupEditor" = "none";
+          "workbench.colorTheme" = "Fleet Dark";
+          "workbench.sideBar.location" = "right";
+
+          "github.copilot.editor.enableAutoCompletions" = true;
+          "github.copilot.nextEditSuggestions.enabled" = true;
+
+          # Go
+          "go.formatTool" = "gofumpt";
+          "go.delveConfig" = {
+            "showGlobalVariables" = true;
+          };
+          "go.lintTool" = "revive";
+          # "go.lintFlags" = [
+          #   "--enable-all"
+          # ];
+          "go.coverOnSingleTest" = true;
+          "go.showWelcome" = true;
+          "gopls" = {
+            "ui.diagnostic.analyses" = {
+              "modernize" = true;
+            };
+            "ui.semanticTokens" = false;
+          };
+
+          "nix.formatterPath" = "alejandra";
+
+          "vim.handleKeys" = {
+            "<C-p>" = false;
+          };
+
+          "solidity.packageDefaultDependenciesContractsDirectory" = "src";
+          "solidity.packageDefaultDependenciesDirectory" = "lib";
+          "solidity.compileUsingRemoteVersion" = "v0.8.23";
+          "solidity.remappings" = [
+            "@openzeppelin/contracts/=lib/openzeppelin-contracts-upgradeable/lib/openzeppelin-contracts/contracts/"
+            "@openzeppelin/contracts-upgradeable/=lib/openzeppelin-contracts-upgradeable/contracts/"
+          ];
+        };
       };
     };
   };
--- home/packages/work.nix
+++ home/packages/work.nix
@@ -32,31 +32,33 @@ in {
     ];
 
     vscode = {
-      extensions = [
-        marketplace.ms-dotnettools.csdevkit
-        marketplace.ms-dotnettools.csharp
-        vscode-dotnet-runtime-fixed
-      ];
-      userSettings = {
-        "csharp.experimental.debug.hotReload" = true;
-        "dotnetAcquisitionExtension.enableTelemetry" = false;
-        "dotnetAcquisitionExtension.sharedExistingDotnetPath" = "${dotnetPackages}/bin/dotnet";
-        "csharp.debug.symbolOptions.cachePath" = "${config.home.homeDirectory}/.cache/vscode-csharp-ls";
-        "csharp.inlayHints.enableInlayHintsForTypes" = true;
-        "csharp.inlayHints.enableInlayHintsForImplicitObjectCreation" = true;
-        "csharp.inlayHints.enableInlayHintsForImplicitVariableTypes" = true;
-        "csharp.inlayHints.enableInlayHintsForLambdaParameterTypes" = true;
-        "dotnet.inlayHints.enableInlayHintsForIndexerParameters" = true;
-        "dotnet.inlayHints.enableInlayHintsForLiteralParameters" = true;
-        "dotnet.inlayHints.enableInlayHintsForObjectCreationParameters" = false;
-        "dotnet.inlayHints.enableInlayHintsForOtherParameters" = true;
-        "dotnet.inlayHints.enableInlayHintsForParameters" = true;
-        "dotnet.inlayHints.suppressInlayHintsForParametersThatDifferOnlyBySuffix" = false;
-        "dotnet.inlayHints.suppressInlayHintsForParametersThatMatchArgumentName" = false;
-        "dotnet.inlayHints.suppressInlayHintsForParametersThatMatchMethodIntent" = false;
-        "dotnet.formatting.organizeImportsOnFormat" = true;
-        "csharp.debug.expressionEvaluationOptions.showRawValues" = true;
-        "omnisharp.enableDecompilationSupport" = true;
+      profiles.default = {
+        extensions = [
+          marketplace.ms-dotnettools.csdevkit
+          marketplace.ms-dotnettools.csharp
+          vscode-dotnet-runtime-fixed
+        ];
+        userSettings = {
+          "csharp.experimental.debug.hotReload" = true;
+          "dotnetAcquisitionExtension.enableTelemetry" = false;
+          "dotnetAcquisitionExtension.sharedExistingDotnetPath" = "${dotnetPackages}/bin/dotnet";
+          "csharp.debug.symbolOptions.cachePath" = "${config.home.homeDirectory}/.cache/vscode-csharp-ls";
+          "csharp.inlayHints.enableInlayHintsForTypes" = true;
+          "csharp.inlayHints.enableInlayHintsForImplicitObjectCreation" = true;
+          "csharp.inlayHints.enableInlayHintsForImplicitVariableTypes" = true;
+          "csharp.inlayHints.enableInlayHintsForLambdaParameterTypes" = true;
+          "dotnet.inlayHints.enableInlayHintsForIndexerParameters" = true;
+          "dotnet.inlayHints.enableInlayHintsForLiteralParameters" = true;
+          "dotnet.inlayHints.enableInlayHintsForObjectCreationParameters" = false;
+          "dotnet.inlayHints.enableInlayHintsForOtherParameters" = true;
+          "dotnet.inlayHints.enableInlayHintsForParameters" = true;
+          "dotnet.inlayHints.suppressInlayHintsForParametersThatDifferOnlyBySuffix" = false;
+          "dotnet.inlayHints.suppressInlayHintsForParametersThatMatchArgumentName" = false;
+          "dotnet.inlayHints.suppressInlayHintsForParametersThatMatchMethodIntent" = false;
+          "dotnet.formatting.organizeImportsOnFormat" = true;
+          "csharp.debug.expressionEvaluationOptions.showRawValues" = true;
+          "omnisharp.enableDecompilationSupport" = true;
+        };
       };
     };
 
