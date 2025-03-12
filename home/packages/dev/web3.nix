59755b8ee29a4ba2f366e1a82d11f5b90daf9db5
Samir Ettali <s.ettali@young.business>
2025-03-12 02:15:54 +0100

    chore: uncommented previously broken slitner-analyzer package

--- home/packages/dev/web3.nix
+++ home/packages/dev/web3.nix
@@ -1,9 +1,9 @@
 {pkgs, ...}: {
   home.packages = with pkgs; [
     foundry-bin
-    solc-select
-    # slither-analyzer
     go-ethereum
     nur.repos.gabr1sr.vscode-solidity-server
+    slither-analyzer
+    solc-select
   ];
 }
