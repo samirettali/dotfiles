890da3f4b3944366c6913c7fc2af2bf33e70dd5f
Samir Ettali <ettali.samir@gmail.com>
2025-03-12 04:55:27 +0100

    refactor(nvim): remove old moonfly overrides

--- home/dotfiles/nvim/lua/plugins/moonfly.lua
+++ home/dotfiles/nvim/lua/plugins/moonfly.lua
@@ -2,12 +2,16 @@ return {
 	"bluz71/vim-moonfly-colors",
 	priority = 1000,
 	config = function()
+		local moonfly = require("moonfly")
+
 		vim.g.moonflyWinSeparator = 2
 		vim.g.moonflyItalics = false
 
-		require("moonfly").custom_colors({
+		local custom_colors = {
 			bg = "#000000",
-		})
+		}
+
+		moonfly.custom_colors(custom_colors)
 
 		vim.api.nvim_create_autocmd("ColorScheme", {
 			pattern = "moonfly",
@@ -15,10 +19,6 @@ return {
 				local palette = require("moonfly").palette
 				vim.api.nvim_set_hl(0, "WinBar", { bg = palette.black, fg = palette.grey247 })
 				vim.api.nvim_set_hl(0, "WinBarNC", { bg = palette.black, fg = palette.grey247 })
-				vim.api.nvim_set_hl(0, "CursorLineNr", { bg = palette.black })
-				vim.api.nvim_set_hl(0, "TroubleNormal", { bg = palette.black })
-				vim.api.nvim_set_hl(0, "TroubleNormalNC", { bg = palette.black })
-				vim.api.nvim_set_hl(0, "FoldColumn", { bg = palette.black, fg = palette.grey241 })
 			end,
 			group = vim.api.nvim_create_augroup("MoonflyHighlight", {}),
 		})
