39af6e4c744fbd79ce3cb3d4af85fa250c3a37d9
Samir Ettali <ettali.samir@gmail.com>
2025-04-12 15:13:38 +0200

    fix(nvim): use new ollama config for avante

--- home/dotfiles/nvim/lua/plugins/avante.lua
+++ home/dotfiles/nvim/lua/plugins/avante.lua
@@ -21,13 +21,8 @@ return {
 	},
 	opts = {
 		provider = "ollama",
-		vendors = {
-			ollama = {
-				__inherited_from = "openai",
-				api_key_name = "",
-				endpoint = "127.0.0.1:11434/v1",
-				model = "qwen2.5-coder",
-			},
+		ollama = {
+			model = "qwen2.5-coder",
 		},
 	},
 }
