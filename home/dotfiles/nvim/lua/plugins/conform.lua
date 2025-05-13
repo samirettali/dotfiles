51ffe4e96b0cbc2c3bb03e6d3699ecd18c39840b
Samir Ettali <ettali.samir@gmail.com>
2025-05-13 10:26:35 +0200

    feat(nvim): use <leader>tf to toggle format on save

--- home/dotfiles/nvim/lua/plugins/conform.lua
+++ home/dotfiles/nvim/lua/plugins/conform.lua
@@ -1,23 +1,43 @@
 return {
 	"stevearc/conform.nvim",
-	opts = {
-		format_on_save = {
-			timeout_ms = 3000,
-			lsp_format = "fallback",
-		},
-		formatters_by_ft = {
-			lua = { "stylua" },
-			go = { "goimports", "gofumpt" },
-			rust = { "rustfmt" },
-			nix = { "alejandra" },
-			javascript = { "prettierd" },
-			python = { "ruff" },
-			ocaml = { "ocamlformat" },
-			cpp = { "clang-format" },
-			cs = { "csharpier" },
-			yaml = { "yamlfmt" },
-			["*"] = { "codespell" },
-			["_"] = { "trim_whitespace" }, -- if no formatter is found, use this
-		},
-	},
+
+	config = function()
+		local opts = {
+			format_on_save = function()
+				if vim.g.disable_autoformat then
+					return
+				end
+
+				return { timeout_ms = 3000, lsp_format = "fallback" }
+			end,
+
+			formatters_by_ft = {
+				lua = { "stylua" },
+				go = { "goimports", "gofumpt" },
+				rust = { "rustfmt" },
+				nix = { "alejandra" },
+				javascript = { "prettierd" },
+				python = { "ruff" },
+				ocaml = { "ocamlformat" },
+				cpp = { "clang-format" },
+				cs = { "csharpier" },
+				yaml = { "yamlfmt" },
+				["*"] = { "codespell" },
+				["_"] = { "trim_whitespace" }, -- fallback
+			},
+		}
+
+		local conform = require("conform")
+		conform.setup(opts)
+
+		vim.keymap.set("n", "<leader>tf", function()
+			vim.g.disable_autoformat = not vim.g.disable_autoformat
+
+			if vim.g.disable_autoformat then
+				vim.notify("Formatting disabled")
+			else
+				vim.notify("Formatting enabled")
+			end
+		end, { desc = "Toggle format on save" })
+	end,
 }
