cb4d816514962ad1af2b1462a1ce5d233e2ab0c7
Samir Ettali <ettali.samir@gmail.com>
2025-06-03 19:32:09 +0200

    feat(llm): set gemini flash as default model

--- home/shell/llm/default.nix
+++ home/shell/llm/default.nix
@@ -14,7 +14,7 @@ in {
   home.packages = [llmWithPlugins];
 
   home.activation.setupLlm = lib.hm.dag.entryAfter ["writeBoundary"] ''
-    ${llmWithPlugins}/bin/llm keys set openrouter \
-      --value "$(cat ${config.sops.secrets."openrouter_api_key".path})"
+    ${llmWithPlugins}/bin/llm keys set openrouter --value "$(cat ${config.sops.secrets."openrouter_api_key".path})"
+    ${llmWithPlugins}/bin//llm models default openrouter/google/gemini-2.5-flash-preview-05-20
   '';
 }
