ba8914c20c6afe1d01b405459e1c666634364021
Samir Ettali <ettali.samir@gmail.com>
2025-06-07 10:58:02 +0200

    fix(flake): do not stop on llm model setting in writeBoundary phase

--- home/shell/llm.nix
+++ home/shell/llm.nix
@@ -13,6 +13,7 @@
 
   home.activation.setupLLM = lib.hm.dag.entryAfter ["writeBoundary"] ''
     ${lib.getExe pkgs.llm} keys set openrouter --value "$(cat ${config.sops.secrets."openrouter_api_key".path})"
-    ${lib.getExe pkgs.llm} models default openrouter/google/gemini-2.5-flash-preview-05-20
+    # TODO: it seems like the openrouter plugins is not available in writeBoundary phase
+    ${lib.getExe pkgs.llm} models default openrouter/google/gemini-2.5-flash-preview-05-20 || true
   '';
 }
