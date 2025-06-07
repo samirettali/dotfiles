ed41f6ef2bdf5a78bec4eea83b7f5c1eb35ac104
Samir Ettali <ettali.samir@gmail.com>
2025-06-07 10:26:50 +0200

    chore(pkgs): disable ollama

--- home/services/ollama.nix
+++ home/services/ollama.nix
@@ -1,46 +1,50 @@
 {
   lib,
   pkgs,
+  config,
   ...
 }: let
   models = [
     "deepcoder"
     "deepseek-r1"
     "gemma3"
+    "gemma3:4b-it-qat"
     "hf.co/bartowski/Qwen2.5.1-Coder-7B-Instruct-GGUF"
     "hf.co/bartowski/Zyphra_ZR1-1.5B-GGUF"
-    "llama3.2"
   ];
+
+  downloadModels = ''
+    total=${toString (builtins.length models)}
+    failed=0
+
+    for model in ${lib.escapeShellArgs models}; do
+      '${lib.getExe pkgs.ollama}' pull "$model" &
+    done
+
+    for job in $(jobs -p); do
+      set +e
+      wait $job
+      exit_code=$?
+      set -e
+
+      if [ $exit_code != 0 ]; then
+        failed=$((failed + 1))
+      fi
+    done
+
+    if [ $failed != 0 ]; then
+      echo "error: $failed out of $total attempted model downloads failed" >&2
+      exit 1
+    fi
+  '';
 in {
   services.ollama = {
-    enable = true;
-    host = "0.0.0.0";
+    enable = false;
   };
 
-  home.activation = {
-    downloadOllamaModels = lib.hm.dag.entryAfter ["writeBoundary"] ''
-      total=${toString (builtins.length models)}
-      failed=0
-
-      for model in ${lib.escapeShellArgs models}; do
-        '${lib.getExe pkgs.ollama}' pull "$model" &
-      done
-
-      for job in $(jobs -p); do
-        set +e
-        wait $job
-        exit_code=$?
-        set -e
-
-        if [ $exit_code != 0 ]; then
-          failed=$((failed + 1))
-        fi
-      done
-
-      if [ $failed != 0 ]; then
-        echo "error: $failed out of $total attempted model downloads failed" >&2
-        exit 1
-      fi
-    '';
-  };
+  home.activation =
+    {}
+    // lib.optionalAttrs config.services.ollama.enable {
+      downloadOllamaModels = lib.hm.dag.entryAfter ["writeBoundary"] downloadModels;
+    };
 }
