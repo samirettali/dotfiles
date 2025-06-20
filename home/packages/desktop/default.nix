11cdcf5c532f118fbac6f92c5cfe8c1364ca6c9c
Samir Ettali <ettali.samir@gmail.com>
2025-06-20 02:24:29 +0200

    fix(pgks): install older keepassxc

--- home/packages/desktop/default.nix
+++ home/packages/desktop/default.nix
@@ -1,4 +1,11 @@
-{pkgs, ...}: {
+{pkgs, ...}: let
+  keepassxcPkgs = import (builtins.fetchGit {
+    name = "keepassxc-pkgs";
+    url = "https://github.com/NixOS/nixpkgs/";
+    ref = "refs/heads/nixpkgs-unstable";
+    rev = "0bd7f95e4588643f2c2d403b38d8a2fe44b0fc73";
+  }) {system = pkgs.system;};
+in {
   imports = [
     ./alacritty.nix
     ./espanso.nix
@@ -12,7 +19,13 @@
 
   home.packages = with pkgs; [
     discord
-    keepassxc
     spotify
   ];
+
+  programs = {
+    keepassxc = {
+      package = keepassxcPkgs.keepassxc; # TODO: remove when upstream is fixed
+      enable = true;
+    };
+  };
 }
