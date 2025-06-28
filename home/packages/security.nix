6ef860a09e393a6cf68af19de1684c6a6b7bb540
Samir Ettali <ettali.samir@gmail.com>
2025-06-28 21:14:21 +0200

    fix(flake): install old version of findomain

--- home/packages/security.nix
+++ home/packages/security.nix
@@ -1,11 +1,23 @@
-{pkgs, ...}: {
+{pkgs, ...}: let
+  findomainPkgs =
+    import (builtins.fetchGit {
+      name = "findomain903";
+      url = "https://github.com/NixOS/nixpkgs/";
+      ref = "refs/heads/nixpkgs-unstable";
+      rev = "0bd7f95e4588643f2c2d403b38d8a2fe44b0fc73";
+    }) {
+      system = pkgs.system;
+    };
+
+  findomain903 = findomainPkgs.findomain;
+in {
   home.packages = with pkgs; [
     # burpsuite
     amass
     cent
     dnsx
     ffuf
-    findomain
+    findomain903 # TODO: upstream is broken
     ghidra-bin
     hakrawler
     httpx
