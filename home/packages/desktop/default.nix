e646b569aa8fad37eca6572e3879a58eb70bb738
Samir Ettali <ettali.samir@gmail.com>
2025-06-29 07:01:22 +0200

    chore: restore upstream keepassxc

--- home/packages/desktop/default.nix
+++ home/packages/desktop/default.nix
@@ -1,11 +1,4 @@
-{pkgs, ...}: let
-  keepassxcPkgs = import (builtins.fetchGit {
-    name = "keepassxc-pkgs";
-    url = "https://github.com/NixOS/nixpkgs/";
-    ref = "refs/heads/nixpkgs-unstable";
-    rev = "0bd7f95e4588643f2c2d403b38d8a2fe44b0fc73";
-  }) {system = pkgs.system;};
-in {
+{pkgs, ...}: {
   imports = [
     ./alacritty.nix
     ./espanso.nix
@@ -25,7 +18,6 @@ in {
 
   programs = {
     keepassxc = {
-      package = keepassxcPkgs.keepassxc; # TODO: remove when upstream is fixed
       enable = true;
     };
   };
