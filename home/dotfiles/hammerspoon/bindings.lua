df76080544b48e9049e5fa9ac67b94db3a3e7724
Samir Ettali <ettali.samir@gmail.com>
2025-07-05 18:07:01 +0200

    chore(hammerspoon): remove spotify bindings

--- home/dotfiles/hammerspoon/bindings.lua
+++ home/dotfiles/hammerspoon/bindings.lua
@@ -1,8 +1,1 @@
-hs.hotkey.bind({ "alt" }, "[", hs.spotify.previous)
-hs.hotkey.bind({ "alt" }, "]", hs.spotify.next)
-hs.hotkey.bind({ "alt" }, "delete", hs.spotify.playpause)
-
-hs.hotkey.bind({ "alt", "shift" }, "[", hs.spotify.rw, hs.spotify.rw, hs.spotify.rw)
-hs.hotkey.bind({ "alt", "shift" }, "]", hs.spotify.ff, hs.spotify.ff, hs.spotify.ff)
-
 hs.hotkey.bind({ "cmd", "shift" }, "l", hs.caffeinate.startScreensaver)
