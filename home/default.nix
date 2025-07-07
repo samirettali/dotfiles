44c327a4381a0f63b523ad09d530b663f4de66d6
Samir Ettali <ettali.samir@gmail.com>
2025-07-07 03:18:19 +0200

    feat(pkgs): add spotify-player

--- home/default.nix
+++ home/default.nix
@@ -5,11 +5,12 @@
   ...
 }: {
   imports = [
-    ./sops.nix
     ./dotfiles.nix
-    ./shell
     ./git-sync.nix
     ./services/default.nix
+    ./shell
+    ./sops.nix
+    ./spotify-player.nix
   ];
 
   home.shell = {
@@ -25,13 +26,6 @@
 
   programs = {
     home-manager.enable = true;
-    ncspot = {
-      enable = true;
-      settings = {
-        shuffle = true;
-        gapless = true;
-      };
-    };
   };
 
   home.packages = with pkgs; [
---
+++ home/spotify-player.nix
@@ -0,0 +1,38 @@
+{
+  pkgs,
+  config,
+  ...
+}: {
+  programs.spotify-player = {
+    # TODO: add sketchybar hooks
+    enable = true;
+    package = pkgs.spotify-player.override {
+      withMediaControl = false;
+    };
+    settings = {
+      theme = "default";
+      client_id = "9f04615abcca4c0d968bd207d2a66215";
+      app_refresh_duration_in_ms = 16;
+      # play_icon = "";
+      # pause_icon = "";
+      # liked_icon = "";
+      seek_duration_secs = 5;
+      default_device = config.home.username;
+      enable_cover_image_cache = true;
+      enable_notify = true;
+      notify_streaming_only = false;
+      enable_streaming = "DaemonOnly";
+      cover_img_length = 9;
+      cover_img_width = 9;
+      device = {
+        name = config.home.username;
+        device_type = "speaker";
+        volume = 100;
+        bitrate = 320;
+        audio_cache = true;
+        normalization = false;
+        autoplay = false;
+      };
+    };
+  };
+}
