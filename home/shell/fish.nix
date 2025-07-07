d4b601b5bfc7b0a0e9b2ecac8efa0d86bcf5e891
Samir Ettali <ettali.samir@gmail.com>
2025-07-07 04:18:34 +0200

    feat(fish): add gum file and history widgets

--- home/shell/fish.nix
+++ home/shell/fish.nix
@@ -19,7 +19,30 @@ in {
 
   programs.fish = {
     enable = true;
-    interactiveShellInit = initFile;
+    interactiveShellInit =
+      initFile
+      + "\n"
+      + ''
+        function gum-file-widget -d "Search files"
+            set -l result (eval ${lib.getExe pkgs.gum} filter)
+            and commandline -rt -- (string join -- ' ' (string escape -- $result))
+
+            commandline -f repaint
+        end
+
+        bind \ct gum-file-widget
+        bind -M insert \ct gum-file-widget
+
+        function gum-history-widget -d "Search history"
+            set -l result (eval history -z --max 100 | ${lib.getExe pkgs.gnused} 's/\x0/<new_line_placeholder>/g' | ${lib.getExe pkgs.gum} filter --input-delimiter '<new_line_placeholder>')
+            and commandline -rt -- $result
+
+            commandline -f repaint
+        end
+
+        bind \cr gum-history-widget
+        bind -M insert \cr gum-history-widget
+      '';
     plugins = [
       {
         name = "grc";
