96389ceb156b50038f4580b30903ec124bd44b78
Samir Ettali <ettali.samir@gmail.com>
2025-07-09 19:20:50 +0200

    feat(pkgs): add pyrefly and ty

--- home/packages/dev/python.nix
+++ home/packages/dev/python.nix
@@ -1,9 +1,11 @@
 {pkgs, ...}: {
   home.packages = with pkgs; [
     basedpyright
+    pyrefly
     pyright
     # python313 # TODO: this is installed along with llm and somehow there's a conflict
     ruff
+    ty
     uv
   ];
 }
