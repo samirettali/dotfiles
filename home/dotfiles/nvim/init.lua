7d100abd0d35030f6bb6b8c14c7245fa6c3061c3
Samir Ettali <ettali.samir@gmail.com>
2025-07-13 03:39:44 +0200

    fix: missing hunk

--- home/dotfiles/nvim/init.lua
+++ home/dotfiles/nvim/init.lua
@@ -24,15 +24,6 @@ local function load_modules()
 end
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.uv.fs_stat(lazypath) then
-	vim.fn.system({
-		"git",
-		"clone",
-		"--filter=blob:none",
-		"https://github.com/folke/lazy.nvim.git",
-		"--branch=stable",
-		lazypath,
-	})
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
 	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
 	local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
