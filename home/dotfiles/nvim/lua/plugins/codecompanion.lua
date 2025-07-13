cca7b127bb5fb4acc13a91a344c53e6c298d7b28
Samir Ettali <ettali.samir@gmail.com>
2025-07-13 03:32:14 +0200

    feat(nvim): add codecompanion plugin

---
+++ home/dotfiles/nvim/lua/plugins/codecompanion.lua
@@ -0,0 +1,29 @@
+return {
+	"olimorris/codecompanion.nvim",
+	cmd = { "CodeCompanion", "CodeCompanionChat", "CodeCompanionActions", "CodeCompanionCmd" },
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-treesitter/nvim-treesitter",
+	},
+	opts = {},
+	keys = {
+		{
+			"<C-a>",
+			"<cmd>CodeCompanionActions<CR>",
+			desc = "Open the action palette",
+			mode = { "n", "v" },
+		},
+		{
+			"<leader>ca",
+			"<cmd>CodeCompanionChat Toggle<CR>",
+			desc = "Toggle a chat buffer",
+			mode = { "n", "v" },
+		},
+		{
+			"<localleader>ca",
+			"<cmd>CodeCompanionChat Add<CR>",
+			desc = "Add code to a chat buffer",
+			mode = { "v" },
+		},
+	},
+}
