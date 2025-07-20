a9bbb90818eaf2d6d79128cb8e39a95045039e20
Samir Ettali <ettali.samir@gmail.com>
2025-07-21 01:51:02 +0200

    nvim: move supermaven to its own file

--- home/dotfiles/nvim/lua/plugins/blink.lua
+++ home/dotfiles/nvim/lua/plugins/blink.lua
@@ -1,83 +1,11 @@
--- TODO: maybe show suggestions and hide them when cmp menu is show with
--- ```
--- vim.api.nvim_create_autocmd("User", {
---   pattern = "BlinkCmpMenuOpen",
---   callback = function()
---     vim.b.copilot_suggestion_hidden = true
---   end,
--- })
---
--- vim.api.nvim_create_autocmd("User", {
---   pattern = "BlinkCmpMenuClose",
---   callback = function()
---     vim.b.copilot_suggestion_hidden = false
---   end,
--- })
--- ```
-
-local dependencies = {
-	"xzbdmw/colorful-menu.nvim",
-	"rafamadriz/friendly-snippets",
-}
-local use_copilot = false
-local source
-
-if use_copilot then
-	source = "copilot"
-	local copilot_deps = {
-		"fang2hou/blink-copilot",
-		{
-			"zbirenbaum/copilot.lua",
-			cmd = "Copilot",
-			event = "InsertEnter",
-			opts = {
-				suggestion = { enabled = false },
-				panel = { enabled = false },
-				server = { type = "binary" },
-				should_attach = function(_, bufname)
-					if string.match(bufname, "env") then
-						return false
-					end
-
-					return true
-				end,
-			},
-		},
-	}
-	for _, v in ipairs(copilot_deps) do
-		table.insert(dependencies, v)
-	end
-else
-	-- TODO: this results as text and not supermaven
-	source = "supermaven"
-	local supermaven_deps = {
-		{
-			"supermaven-inc/supermaven-nvim",
-			event = "InsertEnter",
-			opts = {
-				keymaps = {
-					accept_suggestion = "<C-f>",
-					clear_suggestion = "<C-]>",
-					-- accept_word = "<c-j>",
-				},
-				disable_inline_completion = false,
-				disable_keymaps = false,
-				log_level = "off",
-			},
-		},
-		"huijiro/blink-cmp-supermaven",
-	}
-
-	for _, v in ipairs(supermaven_deps) do
-		table.insert(dependencies, v)
-	end
-end
-
 return {
 	"saghen/blink.cmp",
 	version = "*",
-	event = "InsertEnter", -- Using only insert enter allows blink to work also outside buffers and to not load it if a file is only opened
-	dependencies = dependencies,
+	event = "InsertEnter",
+	dependencies = {
+		"xzbdmw/colorful-menu.nvim",
+		"rafamadriz/friendly-snippets",
+	},
 	---@module 'blink.cmp'
 	---@type blink.cmp.Config
 	opts = {
@@ -86,26 +14,8 @@ return {
 				"lsp",
 				"path",
 				"buffer",
-				-- source,
 				"snippets",
 			},
-			providers = {
-				copilot = {
-					name = "copilot",
-					module = "blink-copilot",
-					score_offset = 100,
-					async = true,
-				},
-				supermaven = {
-					name = "supermaven",
-					module = "blink-cmp-supermaven",
-					score_offset = 150,
-					async = true,
-				},
-			},
-		},
-		appearance = {
-			-- nerd_font_variant = "mono",
 		},
 		signature = {
 			enabled = true,
@@ -123,12 +33,6 @@ return {
 
 			-- ["<Tab>"] = { "snippet_forward", "fallback" },
 			-- ["<S-Tab>"] = { "snippet_backward", "fallback" },
-		},
-		fuzzy = {
-			implementation = "prefer_rust_with_warning",
-			max_typos = 5,
-			use_frecency = true,
-			use_proximity = true,
 		},
 		completion = {
 			documentation = {
---
+++ home/dotfiles/nvim/lua/plugins/supermaven.lua
@@ -0,0 +1,14 @@
+return {
+	"supermaven-inc/supermaven-nvim",
+	event = "InsertEnter",
+	opts = {
+		keymaps = {
+			accept_suggestion = "<C-f>",
+			clear_suggestion = "<C-]>",
+			-- accept_word = "<c-j>",
+		},
+		disable_inline_completion = false,
+		disable_keymaps = false,
+		log_level = "off",
+	},
+}
