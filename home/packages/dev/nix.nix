ce020005519140bca26d2bcac2fffa8d68b4d2b2
Samir Ettali <ettali.samir@gmail.com>
2025-07-21 08:46:08 +0200

    nix: add missing vscode extensions and settings

--- home/packages/dev/nix.nix
+++ home/packages/dev/nix.nix
@@ -4,4 +4,13 @@
     nil
     nixd
   ];
+
+  programs.vscode.profiles.default = {
+    extensions = with pkgs.vscode-marketplace; [
+      jnoortheen.nix-ide
+    ];
+    userSettings = {
+      "nix.formatterPath" = "alejandra";
+    };
+  };
 }
