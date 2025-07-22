b0db5cb94bb8c767dfb623c37102398ec4508e25
Samir Ettali <ettali.samir@gmail.com>
2025-07-23 00:11:21 +0200

    neovim: add dropbar plugin

---
+++ home/dotfiles/nvim/lua/plugins/dropbar.lua
@@ -0,0 +1,16 @@
+return {
+	"Bekaboo/dropbar.nvim",
+	-- TODO: lazy loading might not be needed as the plugin already uses and autocmd
+	-- https://github.com/Bekaboo/dropbar.nvim/blob/master/plugin/dropbar.lua
+	dependencies = {
+		"nvim-tree/nvim-web-devicons",
+	},
+	event = { "BufReadPre", "BufNewFile" },
+	config = function()
+		local dropbar_api = require("dropbar.api")
+		vim.keymap.set("n", "<localleader>;", dropbar_api.pick, { desc = "Pick symbols in winbar" })
+		vim.keymap.set("n", "[;", dropbar_api.goto_context_start, { desc = "Go to start of current context" })
+		vim.keymap.set("n", "];", dropbar_api.select_next_context, { desc = "Select next context" })
+		vim.ui.select = require("dropbar.utils.menu").select
+	end,
+}
