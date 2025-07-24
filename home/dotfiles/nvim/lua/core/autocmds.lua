b5f116d48164c52f304634f036803a5e8e91207f
Samir Ettali <ettali.samir@gmail.com>
2025-07-24 08:18:05 +0200

    neovim: fix lazy winborder

--- home/dotfiles/nvim/lua/core/autocmds.lua
+++ home/dotfiles/nvim/lua/core/autocmds.lua
@@ -27,3 +27,14 @@ vim.api.nvim_create_autocmd({ "BufWritePre", "FileWritePre" }, {
 		end
 	end,
 })
+
+-- TODO: workaround until https://github.com/folke/lazy.nvim/issues/1951 is fixed
+vim.api.nvim_create_autocmd("FileType", {
+	desc = "User: fix backdrop for lazy window",
+	pattern = "lazy_backdrop",
+	group = vim.api.nvim_create_augroup("lazynvim-fix", { clear = true }),
+	callback = function(ctx)
+		local win = vim.fn.win_findbuf(ctx.buf)[1]
+		vim.api.nvim_win_set_config(win, { border = "none" })
+	end,
+})
