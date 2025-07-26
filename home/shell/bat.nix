2d8b8bd833e96806c77d35fbb350b9a79f646701
Samir Ettali <ettali.samir@gmail.com>
2025-07-26 23:36:14 +0200

    bat: remove unused pkgs.fetchFromGithub call

--- home/shell/bat.nix
+++ home/shell/bat.nix
@@ -1,19 +1,19 @@
-{pkgs, ...}: {
+{...}: {
   programs.bat = {
     enable = true;
     config = {
       # theme = "fly16"; # TODO: fix this
     };
     themes = {
-      fly16 = {
-        src = pkgs.fetchFromGitHub {
-          owner = "bluz71";
-          repo = "fly16-bat";
-          rev = "40c20cbb7ec5bf413b24b927952ffc8702b8389b";
-          sha256 = "sha256-IcN5EENhbB82LDb8qpq6EU36eJlpbjKWS5Par0kFCUk=";
-        };
-        file = "fly16.tmTheme";
-      };
+      # fly16 = {
+      #   src = pkgs.fetchFromGitHub {
+      #     owner = "bluz71";
+      #     repo = "fly16-bat";
+      #     rev = "40c20cbb7ec5bf413b24b927952ffc8702b8389b";
+      #     sha256 = "sha256-IcN5EENhbB82LDb8qpq6EU36eJlpbjKWS5Par0kFCUk=";
+      #   };
+      #   file = "fly16.tmTheme";
+      # };
     };
   };
 }
