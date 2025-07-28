9234dbc21b0e0ababf190cc88825cce49dadcecb
Samir Ettali <ettali.samir@gmail.com>
2025-07-28 02:11:23 +0200

    neovim: map <c-q> in mini.pick to send all items to quickfix list

--- home/dotfiles/nvim/lua/plugins/mini.pick.lua
+++ home/dotfiles/nvim/lua/plugins/mini.pick.lua
@@ -2,7 +2,24 @@ return {
 	"echasnovski/mini.pick",
 	event = "VeryLazy",
 	version = false,
-	opts = {},
+	opts = {
+		mappings = {
+			select_all_to_qf = {
+				char = "<C-q>",
+				func = function()
+					local matches = require("mini.pick").get_picker_matches()
+					if not matches or not matches.all or #matches.all == 0 then
+						return false
+					end
+
+					require("mini.pick").default_choose_marked(matches.all)
+
+					-- stop the picker after sending to the quickfix list
+					return true
+				end,
+			},
+		},
+	},
 	keys = {
 		{
 			"<C-f>",
