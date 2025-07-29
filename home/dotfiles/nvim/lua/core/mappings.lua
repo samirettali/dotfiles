e1867d697f8452a8d86723812e2aaf5c45ee640f
Samir Ettali <ettali.samir@gmail.com>
2025-07-29 23:13:59 +0200

    neovim: add keymap to send jumplist to quickfix list

--- home/dotfiles/nvim/lua/core/mappings.lua
+++ home/dotfiles/nvim/lua/core/mappings.lua
@@ -77,4 +77,21 @@ local function toggle_quickfix()
 	vim.cmd.copen()
 end
 
-vim.keymap.set("n", "<localleader>q", toggle_quickfix, { desc = "Toggle Quickfix Window" })
+vim.keymap.set("n", "<leeader>qf", toggle_quickfix, { desc = "Toggle quickfix window" })
+
+vim.keymap.set("n", "<leader>qj", function()
+	local jumplist, _ = unpack(vim.fn.getjumplist())
+	local qf_list = {}
+	for _, v in pairs(jumplist) do
+		if vim.fn.bufloaded(v.bufnr) == 1 then
+			table.insert(qf_list, {
+				bufnr = v.bufnr,
+				lnum = v.lnum,
+				col = v.col,
+				text = vim.api.nvim_buf_get_lines(v.bufnr, v.lnum - 1, v.lnum, false)[1],
+			})
+		end
+	end
+	vim.fn.setqflist(qf_list, " ")
+	vim.cmd("copen")
+end, { desc = "Open jumplist in quickfix window" })
