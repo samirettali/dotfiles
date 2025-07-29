6576538b1d3c105dd08d5328b420c1c092d17e57
Samir Ettali <ettali.samir@gmail.com>
2025-07-29 22:51:16 +0200

    ghostty: set adjust-cell-height to 25%

--- home/packages/desktop/ghostty.nix
+++ home/packages/desktop/ghostty.nix
@@ -23,6 +23,7 @@
       ];
       font-size = 16;
       font-thicken = true;
+      adjust-cell-height = "25%";
       font-thicken-strength = 16;
       gtk-titlebar = false;
       macos-titlebar-proxy-icon = "hidden";
