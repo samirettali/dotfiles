f3107eb1bfb03618e7c99fcfee95d86c70231cc5
Samir Ettali <ettali.samir@gmail.com>
2025-07-31 23:12:50 +0200

    neovim: set highlight_on_yank timeout to 50 ms

--- home/dotfiles/nvim/lua/core/autocmds.lua
+++ home/dotfiles/nvim/lua/core/autocmds.lua
@@ -43,6 +43,6 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 	desc = "Highlight yanked text",
 	group = vim.api.nvim_create_augroup("HighlightYankedText", { clear = true }),
 	callback = function()
-		vim.highlight.on_yank()
+		vim.highlight.on_yank({ timeout = 50 })
 	end,
 })
