b86b320bc21f5355b0a1853ceddfa50a4c4cec21
Samir Ettali <ettali.samir@gmail.com>
2025-08-02 15:38:34 +0200

    neovim: set default model in codecompanion and change keymaps

--- home/dotfiles/nvim/lua/plugins/codecompanion.lua
+++ home/dotfiles/nvim/lua/plugins/codecompanion.lua
@@ -1,35 +1,43 @@
 return {
 	"olimorris/codecompanion.nvim",
-	cmd = { "CodeCompanion", "CodeCompanionChat", "CodeCompanionActions", "CodeCompanionCmd" },
+	cmd = {
+		"CodeCompanion",
+		"CodeCompanionChat",
+		"CodeCompanionActions",
+		"CodeCompanionCmd",
+	},
 	dependencies = {
 		"nvim-lua/plenary.nvim",
 		"nvim-treesitter/nvim-treesitter",
 	},
-	opts = {},
-	keys = {
-		-- {
-		-- 	"<C-a>",
-		-- 	"<cmd>CodeCompanionActions<CR>",
-		-- 	desc = "Open the action palette",
-		-- 	mode = { "n", "v" },
-		-- },
-		{
-			"<leader>ap",
-			":CodeCompanion<cr>",
-			desc = "Open the action palette",
-			mode = { "n", "v" },
+	opts = {
+		strategies = {
+			chat = {
+				adapter = {
+					name = "copilot",
+					model = "claude-sonnet-4-20250514",
+				},
+			},
 		},
+	},
+	keys = {
 		{
-			"<leader>ac",
+			"<localleader>ac",
 			"<cmd>CodeCompanionChat Toggle<CR>",
 			desc = "Toggle a chat buffer",
 			mode = { "n", "v" },
 		},
 		{
-			"<leader>aa",
+			"<localleader>aa",
 			"<cmd>CodeCompanionChat Add<CR>",
 			desc = "Add code to a chat buffer",
 			mode = { "v" },
+		},
+		{
+			"<localleader>ap",
+			"<cmd>CodeCompanionActions<CR>",
+			desc = "Open the action palette",
+			mode = { "n", "v" },
 		},
 	},
 }
