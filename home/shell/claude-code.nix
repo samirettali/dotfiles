e5534a220453691f6a9d0301bad838f7a845c833
Samir Ettali <ettali.samir@gmail.com>
2025-08-03 21:00:02 +0200

    claude: add settings file

--- home/shell/claude-code.nix
+++ home/shell/claude-code.nix
@@ -13,4 +13,15 @@ in {
     ${exe} mcp remove context7 > /dev/null
     ${exe} mcp add --transport http context7 https://mcp.context7.com/mcp > /dev/null
   '';
+
+  # TODO: https://docs.anthropic.com/en/docs/claude-code/settings
+  home.file = {
+    ".claude/settings.json".text = builtins.toJSON {
+      includeCoAuthoredBy = false;
+      env = {
+        CLAUDE_CODE_IDE_SKIP_AUTO_INSTALL = "1";
+        CLAUDE_CODE_DISABLE_NONESSENTIAL_TRAFFIC = "1";
+      };
+    };
+  };
 }
