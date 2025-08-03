8a9de67a0692641e835be1c4ce94a520900ff3f8
Samir Ettali <ettali.samir@gmail.com>
2025-08-03 21:01:09 +0200

    spotify-player: disable notifications and restore default refresh rate

--- home/spotify-player/default.nix
+++ home/spotify-player/default.nix
@@ -11,16 +11,17 @@ in {
       withMediaControl = false;
       withImage = false; # TODO: this is broken inside zellij/ghostty
       withSixel = false;
+      withNotify = false;
     };
     settings = {
       theme = "default";
       client_id = "9f04615abcca4c0d968bd207d2a66215";
       app_refresh_duration_in_ms = 16;
-      playback_refresh_duration_in_ms = 1000;
+      # playback_refresh_duration_in_ms = 1000; # TODO: rate limit
       seek_duration_secs = 5;
       default_device = config.home.username;
       enable_cover_image_cache = true;
-      enable_notify = true;
+      enable_notify = false;
       client_port = 9999;
       notify_streaming_only = false;
       enable_streaming = "DaemonOnly";
