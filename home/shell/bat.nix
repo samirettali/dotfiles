26ef0af481eba03c6431981b02e865a0f84b98a3
Samir Ettali <ettali.samir@gmail.com>
2025-08-03 21:00:17 +0200

    bat: use base16 theme

--- home/shell/bat.nix
+++ home/shell/bat.nix
@@ -2,18 +2,9 @@
   programs.bat = {
     enable = true;
     config = {
-      # theme = "fly16"; # TODO: fix this
-    };
-    themes = {
-      # fly16 = {
-      #   src = pkgs.fetchFromGitHub {
-      #     owner = "bluz71";
-      #     repo = "fly16-bat";
-      #     rev = "40c20cbb7ec5bf413b24b927952ffc8702b8389b";
-      #     sha256 = "sha256-IcN5EENhbB82LDb8qpq6EU36eJlpbjKWS5Par0kFCUk=";
-      #   };
-      #   file = "fly16.tmTheme";
-      # };
+      theme = "base16";
+      style = "numbers,changes,header";
+      italic-text = "never";
     };
   };
 }
