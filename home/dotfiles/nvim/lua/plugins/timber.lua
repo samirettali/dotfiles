555cfb8d7a73a9258eb4588e667aa2466e135c0f
Samir Ettali <ettali.samir@gmail.com>
2025-08-04 12:34:42 +0200

    neovim: add timber timed log

--- home/dotfiles/nvim/lua/plugins/timber.lua
+++ home/dotfiles/nvim/lua/plugins/timber.lua
@@ -1,5 +1,28 @@
 return {
 	"Goose97/timber.nvim",
 	event = "VeryLazy",
-	opts = {},
+	opts = {
+		log_templates = {
+			time_start = {
+				lua = [[local _start = os.time()]],
+				go = [[_start := time.Now()]],
+			},
+			time_end = {
+				lua = [[print("Elapsed time: " .. tostring(os.time() - _start) .. " seconds")]],
+				go = [[fmt.Printf("Elapsed time: %d ms\n", time.Since(_start).Milliseconds())]],
+			},
+		},
+	},
+	keys = {
+		{
+			"glt",
+			function()
+				require("timber.actions").insert_log({
+					templates = { before = "time_start", after = "time_end" },
+					position = "surround",
+				})
+			end,
+			{ desc = "Timber: insert timed log" },
+		},
+	},
 }
