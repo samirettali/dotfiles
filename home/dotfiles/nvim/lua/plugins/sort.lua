0666d76e35cb02368943576d1e8c40f4fc36a19b
Samir Ettali <ettali.samir@gmail.com>
2025-08-04 12:52:23 +0200

    neovim: fix sort.nvim mappings

--- home/dotfiles/nvim/lua/plugins/sort.lua
+++ home/dotfiles/nvim/lua/plugins/sort.lua
@@ -1,5 +1,17 @@
 return {
 	"sQVe/sort.nvim",
-	opts = {},
 	event = { "BufReadPre", "BufNewFile" },
+	opts = {
+		mappings = {
+			operator = "gs", -- this overrides the default gs mapping (goto sleep) but i never use it
+			textobject = {
+				inner = "is",
+				around = "as",
+			},
+			motion = {
+				next_delimiter = "]s",
+				prev_delimiter = "[s",
+			},
+		},
+	},
 }
