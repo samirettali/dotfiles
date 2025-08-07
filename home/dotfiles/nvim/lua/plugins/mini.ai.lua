19f76add33e9df05834912d0905c59e2e9205cb2
Samir Ettali <ettali.samir@gmail.com>
2025-08-08 00:02:09 +0200

    neovim: add mini.ai plugin

---
+++ home/dotfiles/nvim/lua/plugins/mini.ai.lua
@@ -0,0 +1,17 @@
+return {
+	"echasnovski/mini.ai",
+	dependencies = { "echasnovski/mini.extra" },
+	event = "VeryLazy",
+	config = function()
+		local gen_ai_spec = require("mini.extra").gen_ai_spec
+		require("mini.ai").setup({
+			custom_textobjects = {
+				e = gen_ai_spec.buffer(),
+				D = gen_ai_spec.diagnostic(),
+				I = gen_ai_spec.indent(),
+				L = gen_ai_spec.line(),
+				N = gen_ai_spec.number(),
+			},
+		})
+	end,
+}
