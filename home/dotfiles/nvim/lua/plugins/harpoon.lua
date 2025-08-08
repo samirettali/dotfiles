7c7acab7f447b741c558c5b3e4fd686d6b062f04
Samir Ettali <ettali.samir@gmail.com>
2025-08-09 00:22:22 +0200

    neovim: add harpoon plugin

---
+++ home/dotfiles/nvim/lua/plugins/harpoon.lua
@@ -0,0 +1,38 @@
+return {
+	"ThePrimeagen/harpoon",
+	branch = "harpoon2",
+	dependencies = { "nvim-lua/plenary.nvim" },
+	event = "VeryLazy",
+	config = function()
+		local harpoon = require("harpoon")
+
+		harpoon:setup()
+
+		vim.keymap.set("n", "<leader>a", function()
+			harpoon:list():add()
+		end)
+		vim.keymap.set("n", "<C-e>", function()
+			harpoon.ui:toggle_quick_menu(harpoon:list())
+		end)
+
+		vim.keymap.set("n", "<C-h>", function()
+			harpoon:list():select(1)
+		end)
+		vim.keymap.set("n", "<C-j>", function()
+			harpoon:list():select(2)
+		end)
+		vim.keymap.set("n", "<C-k>", function()
+			harpoon:list():select(3)
+		end)
+		vim.keymap.set("n", "<C-l>", function()
+			harpoon:list():select(4)
+		end)
+
+		vim.keymap.set("n", "<C-S-P>", function()
+			harpoon:list():prev()
+		end)
+		vim.keymap.set("n", "<C-S-N>", function()
+			harpoon:list():next()
+		end)
+	end,
+}
