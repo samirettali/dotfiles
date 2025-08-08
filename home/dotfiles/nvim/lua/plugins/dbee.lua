4059a68b2bb852f5bfa533c4666fcb809bed4d72
Samir Ettali <ettali.samir@gmail.com>
2025-08-09 00:22:07 +0200

    neovim: add dbee plugin

---
+++ home/dotfiles/nvim/lua/plugins/dbee.lua
@@ -0,0 +1,15 @@
+return {
+	"kndndrj/nvim-dbee",
+	dependencies = {
+		"MunifTanjim/nui.nvim",
+	},
+	event = "VeryLazy",
+	opts = {
+		drawer = {
+			disable_help = true,
+		},
+		result = {
+			focus_result = true,
+		},
+	},
+}
