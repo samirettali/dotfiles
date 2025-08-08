752329928f08b173de17ece43e241dd3fa95c842
Samir Ettali <ettali.samir@gmail.com>
2025-08-08 17:28:08 +0200

    neovim: change treesitter incremental_selection keymaps

--- home/dotfiles/nvim/lua/plugins/treesitter.lua
+++ home/dotfiles/nvim/lua/plugins/treesitter.lua
@@ -32,10 +32,10 @@ return {
 			incremental_selection = {
 				enable = true,
 				keymaps = {
-					init_selection = "<leader>vv",
-					node_incremental = "+",
-					scope_incremental = false,
-					node_decremental = "_",
+					init_selection = "gnn",
+					node_incremental = "grn",
+					scope_incremental = "grc",
+					node_decremental = "grm",
 				},
 			},
 			context_commentstring = {
