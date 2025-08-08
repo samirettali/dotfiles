61957e9bdb094e2250e27f26f1baa3919b862556
Samir Ettali <ettali.samir@gmail.com>
2025-08-09 00:41:02 +0200

    neovim: use mini.notify as default notification provider

--- home/dotfiles/nvim/lua/plugins/mini.notify.lua
+++ home/dotfiles/nvim/lua/plugins/mini.notify.lua
@@ -2,5 +2,10 @@ return {
 	"echasnovski/mini.notify",
 	version = false,
 	event = "VeryLazy",
-	opts = {},
+	config = function()
+		local notify = require("mini.notify")
+		notify.setup()
+
+		vim.notify = notify.make_notify()
+	end,
 }
