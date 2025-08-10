0f671600c9dfa95729e7309bfded5b10e62eeecf
Samir Ettali <ettali.samir@gmail.com>
2025-08-10 04:17:09 +0200

    neovim: remove completion trigger on every char

--- home/dotfiles/nvim/lua/core/lsp.lua
+++ home/dotfiles/nvim/lua/core/lsp.lua
@@ -48,13 +48,6 @@ vim.api.nvim_create_autocmd("LspAttach", {
 		end
 
 		if client:supports_method(methods.textDocument_completion) then
-			local chars = {}
-			for i = 32, 126 do
-				table.insert(chars, string.char(i))
-			end
-
-			client.server_capabilities.completionProvider.triggerCharacters = chars
-
 			vim.lsp.completion.enable(true, client.id, ev.buf, {
 				autotrigger = true,
 				convert = function(item)
