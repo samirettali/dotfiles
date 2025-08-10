35d62b5c3b54e3b0767fbb81ebe4fe6fd2b2a850
Samir Ettali <ettali.samir@gmail.com>
2025-08-10 19:39:55 +0200

    neovim: keymaps cleanup

--- home/dotfiles/nvim/lua/core/keymaps.lua
+++ home/dotfiles/nvim/lua/core/keymaps.lua
@@ -7,17 +7,14 @@ vim.keymap.set("n", "<Leader>tl", "<CMD>set list!<CR>", { desc = "Toggle listcha
 vim.keymap.set("n", "<Leader>tc", "<CMD>set cursorline!<CR>", { desc = "Toggle cursorline" })
 
 vim.keymap.set("n", "<localleader>q", utils.toggle_quickfix, { desc = "Toggle quickfix window" })
-vim.keymap.set("n", "<localleader>r", "<CMD>restart<CR>", { desc = "Restart" })
-
-vim.keymap.set({ "n", "v" }, "dd", function()
-	if vim.api.nvim_get_current_line():match("^%s*$") then
-		vim.cmd('normal! "_dd')
-	else
-		vim.cmd("normal! dd")
-	end
-end, { desc = "Delete a line and copy it only if it's not empty", silent = true })
 
 vim.keymap.set("n", "<localleader>c", utils.delete_hidden_buffers, { desc = "Close hidden buffers" })
+
+vim.keymap.set({ "i", "n" }, "<esc>", "<CMD>noh<CR><esc>", { desc = "Clear hlsearch and ESC" })
+
+-- TODO: overrides default behaviour
+-- vim.keymap.set("x", "/", "<Esc>/\\%V", { desc = "Search within visual selection" })
+-- vim.keymap.set("n", "J", "mzJ`z:delmarks z<CR>", { desc = "Keep cursor in place when joining lines" })
 
 vim.keymap.set({ "n", "x" }, "yc", function()
 	vim.opt.operatorfunc = [[v:lua.require("core.utils").duplicate_and_comment_lines]]
@@ -29,17 +26,12 @@ vim.keymap.set("n", "ycc", function()
 	return "g@_"
 end, { expr = true, desc = "Duplicate [count] lines and comment out the first instance" })
 
--- TODO: overrides default behaviour
--- vim.keymap.set("x", "/", "<Esc>/\\%V", { desc = "Search within visual selection" })
--- vim.keymap.set("n", "J", "mzJ`z:delmarks z<CR>", { desc = "Keep cursor in place when joining lines" })
+vim.keymap.set({ "n", "v" }, "dd", function()
+	if vim.api.nvim_get_current_line():match("^%s*$") then
+		vim.cmd('normal! "_dd')
+	else
+		vim.cmd("normal! dd")
+	end
+end, { desc = "Delete a line and copy it only if it's not empty", silent = true })
 
-vim.keymap.set("x", "R", ":s###g<left><left><left>", { desc = "Start replacement in the visual selected region" })
-
-vim.keymap.set({ "i", "n" }, "<esc>", "<CMD>noh<CR><esc>", { desc = "Clear hlsearch and ESC" })
-
-vim.keymap.set(
-	"n",
-	"<localleader>s",
-	"<CMD>source %<CR>",
-	{ noremap = true, silent = true, desc = "Source current file" }
-)
+vim.keymap.set("n", "<localleader>r", "<CMD>restart<CR>", { desc = "Restart" })
