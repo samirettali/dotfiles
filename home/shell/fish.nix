c58b42c3fc414899762a1c068a970b90e88e5e65
Samir Ettali <ettali.samir@gmail.com>
2025-08-10 19:52:37 +0200

    fish: remove right prompt

--- home/shell/fish.nix
+++ home/shell/fish.nix
@@ -9,16 +9,23 @@
         fish
         */
         ''
+          set -l last_status $status # this has to be here otherwise $status will be overridden by any command
+
           # Select symbol
           set -l symbol '$ '
           if fish_is_root_user
-              set symbol '# '
+          	set symbol '# '
+          end
+
+          set -l stat
+          if test $last_status -ne 0
+          	set stat (set_color -o bryellow)"($last_status) "(set_color normal)
           end
 
           # Show user@host if connected via SSH
           set -l ssh
           if set -q SSH_CLIENT || set -q SSH_TTY
-              set ssh (set_color -o blue) $USER '@' (hostname) (set_color -o normal) ":" (set_color normal)
+          	set ssh (set_color -o blue) $USER '@' (hostname) (set_color -o normal) ":" (set_color normal)
           end
 
           set -l pwd (set_color -o blue) (prompt_pwd) (set_color normal)
@@ -26,53 +33,10 @@
 
           set -l nix_shell_info
           if test -n "$IN_NIX_SHELL"
-              set nix_shell_info "<nix-shell> "
+          	set nix_shell_info "<nix-shell> "
           end
 
-          printf (string join "" -- $ssh $nix_shell_info $pwd $symbol)
-        '';
-      fish_right_prompt =
-        /*
-        fish
-        */
-        ''
-          # Show last status code if != 0
-          set -l last_status $status
-
-          set -l stat
-          if test $last_status -ne 0
-              set stat (set_color -o bryellow)"($last_status)"(set_color normal)
-          end
-
-          # Git prompt
-          set -l gp (set_color -o brmagenta)(fish_git_prompt "[%s]")(set_color normal)
-
-          # Duration
-          set -l d $CMD_DURATION
-          set -l second 1000
-          set -l minute (math 60 \* $second)
-          set -l hour (math $minute \* 60)
-          set -l s (math -s0 $d / $second)
-          set -l m (math -s0 $d / $minute)
-          set -l h (math -s0 $d / $hour)
-          set -l duration
-
-          if test $h -gt 0
-              set h (math -s2 $d / $hour)
-              set duration $h'h'
-          else if test $m -gt 0
-              set m (math -s2 $d / $minute)
-              set duration $m'm'
-          else if test $s -gt 0
-              set s (math -s2 $d / $second)
-              set duration $s's'
-          else
-              set duration $d'ms'
-          end
-
-          set duration (set_color -id white)$duration(set_color normal)
-
-          printf (string join ' ' -- $duration $stat $gp)
+          printf (string join "" -- $stat $ssh $nix_shell_info $pwd $symbol)
         '';
     };
     interactiveShellInit =
@@ -83,31 +47,10 @@
         set fish_color_command green
         set fish_color_valid_path normal
 
-        if not set -q NVIM
-            set -g fish_key_bindings fish_hybrid_key_bindings
-        end
+        set -g fish_key_bindings fish_hybrid_key_bindings
 
         # TODO: this is not working
         # bind \cz fg
-
       '';
-    plugins = [
-      {
-        name = "grc";
-        src = pkgs.fishPlugins.grc.src;
-      }
-      {
-        name = "done";
-        src = pkgs.fishPlugins.done.src;
-      }
-      # TODO
-      # {
-      #   name = "async_prompt";
-      #   src = pkgs.fishPlugins.async-prompt.src;
-      # }
-    ];
   };
-  home.packages = with pkgs; [
-    grc
-  ];
 }
