118904f27e48c2a62c7d26b167f6f14fc83c2ff8
Samir Ettali <ettali.samir@gmail.com>
2025-08-10 02:47:23 +0200

    neovim: set winblend to 0 in mini.notify

--- home/dotfiles/nvim/lua/plugins/mini.notify.lua
+++ home/dotfiles/nvim/lua/plugins/mini.notify.lua
@@ -2,9 +2,14 @@ return {
 	"echasnovski/mini.notify",
 	version = false,
 	event = "VeryLazy",
-	config = function()
+	opts = {
+		window = {
+			winblend = 0,
+		},
+	},
+	config = function(_, opts)
 		local notify = require("mini.notify")
-		notify.setup()
+		notify.setup(opts)
 
 		vim.notify = notify.make_notify()
 	end,
