3ce1371f192cc66836265557a07dd45061a7c4bc
Samir Ettali <ettali.samir@gmail.com>
2025-08-10 18:23:15 +0200

    neovim: add keymap to toggle copilot

--- home/dotfiles/nvim/lua/plugins/copilot.lua
+++ home/dotfiles/nvim/lua/plugins/copilot.lua
@@ -13,4 +13,20 @@ return {
 			},
 		},
 	},
+	keys = {
+		{
+			"<leader>ts",
+			function()
+				local client = require("copilot.client")
+				if client.is_disabled() then
+					require("copilot.command").enable()
+					vim.notify("Copilot enabled", vim.log.levels.INFO)
+				else
+					require("copilot.command").disable()
+					vim.notify("Copilot disabled", vim.log.levels.INFO)
+				end
+			end,
+			desc = "Toggle Copilot",
+		},
+	},
 }
