96ab0d39f9d8d4d6e98c2cd2bb41724e0a9c869c
Samir Ettali <ettali.samir@gmail.com>
2025-08-14 02:06:32 +0200

    claude: remove home.activation step and direcly write to settings file

--- home/shell/claude-code.nix
+++ home/shell/claude-code.nix
@@ -9,13 +9,6 @@ in {
     claude-code
   ];
 
-  home.activation.setupClaudeCode = lib.hm.dag.entryAfter ["writeBoundary"] ''
-    ${exe} mcp remove context7 || true > /dev/null
-    ${exe} mcp remove playwright || true > /dev/null
-    ${exe} mcp add --scope user --transport http context7 https://mcp.context7.com/mcp > /dev/null
-    ${exe} mcp add --scope user playwright npx "@playwright/mcp@latest" > /dev/null
-  '';
-
   # TODO: https://docs.anthropic.com/en/docs/claude-code/settings
   home.file = {
     ".claude/settings.json".text = builtins.toJSON {
@@ -24,6 +17,32 @@ in {
         command = "bunx ccusage statusline";
       };
       includeCoAuthoredBy = false;
+      mcpServers = {
+        context7 = {
+          type = "http";
+          url = "https://mcp.context7.com/mcp";
+        };
+        playwright = {
+          type = "stdio";
+          command = "npx";
+          args = [
+            "@playwright/mcp@latest"
+          ];
+          env = {
+          };
+        };
+        ast-grep = {
+          type = "stdio";
+          command = "uvx";
+          args = [
+            "--from"
+            "git+https://github.com/ast-grep/ast-grep-mcp"
+            "ast-grep-server"
+          ];
+          env = {
+          };
+        };
+      };
       env = {
         CLAUDE_CODE_IDE_SKIP_AUTO_INSTALL = "1";
         CLAUDE_CODE_DISABLE_NONESSENTIAL_TRAFFIC = "1";
