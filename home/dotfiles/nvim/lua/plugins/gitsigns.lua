b07b47621dca6b611c833b53ea3c3a70fd0b9a87
Samir Ettali <ettali.samir@gmail.com>
2025-08-16 19:25:14 +0200

    neovim: change gitsigns keymaps

--- home/dotfiles/nvim/lua/plugins/gitsigns.lua
+++ home/dotfiles/nvim/lua/plugins/gitsigns.lua
@@ -31,20 +31,20 @@ return {
 			end
 		end)
 
-		vim.keymap.set("n", "<leader>hr", gitsigns.reset_hunk)
-		vim.keymap.set("n", "<leader>hs", gitsigns.stage_hunk)
-		vim.keymap.set("v", "<leader>hr", function()
+		vim.keymap.set("n", "<leader>gr", gitsigns.reset_hunk)
+		vim.keymap.set("n", "<leader>gs", gitsigns.stage_hunk)
+		vim.keymap.set("v", "<leader>gr", function()
 			gitsigns.reset_hunk({ vim.fn.line("."), vim.fn.line("v") })
 		end)
-		vim.keymap.set("v", "<leader>hs", function()
+		vim.keymap.set("v", "<leader>gs", function()
 			gitsigns.stage_hunk({ vim.fn.line("."), vim.fn.line("v") })
 		end)
-		vim.keymap.set("n", "<leader>hS", gitsigns.stage_buffer)
+		vim.keymap.set("n", "<leader>gS", gitsigns.stage_buffer)
 		-- vim.keymap.set("n", "<leader>hR", gitsigns.reset_buffer) -- TODO: this is dangerous
 
-		vim.keymap.set("n", "<leader>hu", gitsigns.undo_stage_hunk)
-		vim.keymap.set("n", "<leader>hp", gitsigns.preview_hunk_inline)
-		vim.keymap.set("n", "<leader>hd", gitsigns.diffthis) -- TODO: how to quit?
+		vim.keymap.set("n", "<leader>gu", gitsigns.undo_stage_hunk)
+		vim.keymap.set("n", "<leader>gp", gitsigns.preview_hunk_inline)
+		vim.keymap.set("n", "<leader>gd", gitsigns.diffthis) -- TODO: how to quit?
 		-- vim.keymap.set("n", "<leader>td", gitsigns.toggle_deleted) -- TODO
 
 		vim.keymap.set("n", "<leader>tg", gitsigns.toggle_current_line_blame)
