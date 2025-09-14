f61af147699f305e99de811e71b798365699fbe6
Samir Ettali <ettali.samir@gmail.com>
2025-09-14 12:34:38 +0200

    neohtop: init

--- darwin/homebrew.nix
+++ darwin/homebrew.nix
@@ -16,6 +16,7 @@
       "docker-desktop"
       "eqmac"
       "maccy"
+      "neohtop"
       "shottr"
       "sloth"
       "sol"
--- home/linux/desktop/default.nix
+++ home/linux/desktop/default.nix
@@ -11,6 +11,7 @@
     ledger-live-desktop
     lmstudio
     redisinsight
+    neohtop
   ];
 
   # TODO:
