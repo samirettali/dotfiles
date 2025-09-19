c03fe8a85bb7ee3b253069065f0e635a91df3d84
Samir Ettali <ettali.samir@gmail.com>
2025-09-19 22:50:55 +0200

    neovim: show hidden files in oil

--- home/dotfiles/nvim/lua/plugins/oil.lua
+++ home/dotfiles/nvim/lua/plugins/oil.lua
@@ -9,6 +9,9 @@ return {
 		default_file_explorer = true,
 		delete_to_trash = true,
 		skip_confirm_for_simple_edits = true,
+		view_options = {
+			show_hidden = false,
+		},
 	},
 	event = "VeryLazy",
 	keys = {
