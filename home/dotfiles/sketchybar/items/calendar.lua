af0899473e76158fd41047cd63f487cf58d61cfb
Samir Ettali <ettali.samir@gmail.com>
2025-10-05 03:23:11 +0200

    sketchybar: cleanup

--- home/dotfiles/sketchybar/items/calendar.lua
+++ home/dotfiles/sketchybar/items/calendar.lua
@@ -1,12 +1,11 @@
 local icons = require("icons")
-local colors = require("colors")
 
 local cal = sbar.add("item", {
 	position = "right",
 	update_freq = 1,
 })
 
-cal:subscribe({ "forced", "routine", "system_woke" }, function(env)
+cal:subscribe({ "forced", "routine", "system_woke" }, function(_)
 	local date = os.date("%a %d")
 	local time = os.date("%H:%M:%S")
 	local label = ("%s - %s"):format(date, time)
