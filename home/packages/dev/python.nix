b565cd3809bf9c61e10995d7207a186aff263bac
Samir Ettali <ettali.samir@gmail.com>
2025-10-28 17:20:48 +0100

    python: optionally install vscode extensions

--- home/packages/dev/python.nix
+++ home/packages/dev/python.nix
@@ -1,25 +1,44 @@
-{pkgs, ...}: {
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}: {
   home.packages = with pkgs; [
     python313Packages.debugpy # used by neovim dap
     basedpyright
     pyrefly
-    pyright
-    python313
+    python314
     ruff
     ty
     uv
   ];
 
-  # TODO: handle optionals
+  programs = {
+    ruff = {
+      enable = true;
+      settings = {};
+    };
+  };
+
   programs.vscode.profiles.default = {
-    extensions = with pkgs.vscode-marketplace; [
-      astral-sh.ty
-      charliermarsh.ruff
-      detachhead.basedpyright
-      meta.pyrefly
-      ms-python.debugpy
-      ms-python.python
-    ];
+    extensions = with pkgs.vscode-marketplace;
+      [
+        ms-python.debugpy
+        ms-python.python
+      ]
+      ++ lib.optionals (builtins.elem pkgs.basedpyright config.home.packages) [
+        detachhead.basedpyright
+      ]
+      ++ lib.optionals (builtins.elem pkgs.pyrefly config.home.packages) [
+        meta.pyrefly
+      ]
+      ++ lib.optionals config.programs.ruff.enable [
+        charliermarsh.ruff
+      ]
+      ++ lib.optionals (builtins.elem pkgs.ty config.home.packages) [
+        astral-sh.ty
+      ];
     userSettings = {
       "files.exclude" = {
         "**/__pycache__" = true;
