efe123ea4e40f87f0a1057ab6b3e6a4fe04bdd6d
Samir Ettali <ettali.samir@gmail.com>
2025-12-04 14:59:59 +0100

    gemini-cli: enable preview features

--- home/shell/gemini-cli.nix
+++ home/shell/gemini-cli.nix
@@ -1,25 +1,21 @@
-{pkgs, ...}: {
+{...}: {
   programs.gemini-cli = {
     enable = true;
     settings = {
       preferredEditor = "vim";
       vimMode = true;
+      previewFeatures = true;
       checkpointing = {
         enabled = true;
       };
-      hideTips = true;
-      hideBanner = true;
+      hideTips = false;
+      hideBanner = false;
       usageStatisticsEnabled = false;
       telemetry = {
         enabled = false;
       };
       # contextFileName = "AGENTS.md";
       selectedAuthType = "oauth-personal";
-      # mcpServers = {
-      # context7 = {
-      #   httpUrl = "https://mcp.context7.com/mcp";
-      # };
-      # };
     };
   };
 }
