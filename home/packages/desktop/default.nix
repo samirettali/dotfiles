adafb7700248f29d425a12961091e1ceb7ef4a33
Samir Ettali <ettali.samir@gmail.com>
2026-02-27 12:45:27 +0100

    obsidian: init

--- home/packages/desktop/default.nix
+++ home/packages/desktop/default.nix
@@ -1,17 +1,18 @@
 {pkgs, ...}: {
   imports = [
     ./alacritty.nix
+    ./cursor.nix
+    ./discord.nix
     ./espanso.nix
     ./firefox
-    ./cursor.nix
     ./ghostty.nix
     ./keepassxc.nix
     ./kitty.nix
     ./mpv.nix
+    ./obsidian.nix
     ./vscode.nix
     ./wezterm.nix
     ./zed.nix
-    ./discord.nix
   ];
 
   home.packages = with pkgs; [
---
+++ home/packages/desktop/obsidian.nix
@@ -0,0 +1,5 @@
+{...}: {
+  programs.obsidian = {
+    enable = true;
+  };
+}
