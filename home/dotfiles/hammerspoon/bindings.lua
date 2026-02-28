7a9cc7484192503f32f63c0c985ab9d074e61747
Samir Ettali <ettali.samir@gmail.com>
2026-02-28 16:11:22 +0100

    hammerspoon: add spotify_player bindings

--- home/dotfiles/hammerspoon/bindings.lua
+++ home/dotfiles/hammerspoon/bindings.lua
@@ -1,4 +1,11 @@
 local lib = require("lib")
+local spotify = require("spotify")
 
 hs.hotkey.bind({ "cmd", "shift" }, "l", hs.caffeinate.startScreensaver)
 hs.hotkey.bind({ "cmd", "ctrl" }, "l", lib.toggleLayout)
+
+hs.hotkey.bind({ "alt" }, "delete", spotify.play_pause)
+hs.hotkey.bind({ "alt" }, "[", spotify.prev)
+hs.hotkey.bind({ "alt" }, "]", spotify.next)
+hs.hotkey.bind({ "alt", "shift" }, "[", spotify.rw, nil, spotify.rw)
+hs.hotkey.bind({ "alt", "shift" }, "]", spotify.ff, nil, spotify.ff)
