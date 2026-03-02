ce1fafb1e9e7ff21d32f88c661c554ae0d2c5606
Samir Ettali <ettali.samir@gmail.com>
2026-03-02 22:51:23 +0100

    neovim: remove highlight-undo

--- home/dotfiles/nvim/lua/plugins/highlight-undo.lua
+++
@@ -1,9 +0,0 @@
-return {
-	"tzachar/highlight-undo.nvim",
-	event = { "BufReadPre", "BufNewFile" },
-	opts = {
-		hlgroup = "HighlightUndo",
-		duration = 50,
-		ignored_filetypes = { "neo-tree", "fugitive", "TelescopePrompt", "mason", "lazy" },
-	},
-}
