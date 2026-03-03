655ac53229aad5b61af7c0959535d1e5be065552
Samir Ettali <ettali.samir@gmail.com>
2026-03-03 21:44:35 +0100

    neovim: use alejandra as formatter in nixd

--- home/dotfiles/nvim/lsp/nixd.lua
+++ home/dotfiles/nvim/lsp/nixd.lua
@@ -2,4 +2,11 @@ return {
 	cmd = { "nixd" },
 	filetypes = { "nix" },
 	root_markers = { "flake.nix", ".git" },
+	settings = {
+		nixd = {
+			formatting = {
+				command = { "alejandra" },
+			},
+		},
+	},
 }
