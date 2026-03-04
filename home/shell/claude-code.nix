003848966acd127e9ef29f1fd0a82b0f202905b1
Samir Ettali <ettali.samir@gmail.com>
2026-03-04 02:28:39 +0100

    claude: fix hooks

--- home/shell/claude-code.nix
+++ home/shell/claude-code.nix
@@ -13,65 +13,38 @@
         DISABLE_AUTOUPDATER = "1";
       };
     };
-    # hooks = {
-    # Notification = lib.optionals pkgs.stdenv.hostPlatform.isDarwin [
-    #   {
-    #     matcher = "";
-    #     hooks = [
-    #       {
-    #         type = "command";
-    #         command = "afplay /System/Library/Sounds/Blow.aiff";
-    #       }
-    #     ];
-    #   }
-    # ];
-    # Stop = lib.optionals pkgs.stdenv.hostPlatform.isDarwin [
-    #   {
-    #     matcher = "";
-    #     hooks = [
-    #       {
-    #         type = "command";
-    #         command = "afplay /System/Library/Sounds/Hero.aiff";
-    #       }
-    #     ];
-    #   }
-    # ];
-    # };
-    # mcpServers = {
-    # context7 = {
-    #   type = "http";
-    #   url = "https://mcp.context7.com/mcp";
-    # };
-    # playwright = {
-    #   type = "stdio";
-    #   command = "npx";
-    #   args = [
-    #     "@playwright/mcp@latest"
-    #   ];
-    #   env = {
-    #   };
-    # };
-    # ast-grep = {
-    #   type = "stdio";
-    #   command = "uvx";
-    #   args = [
-    #     "--from"
-    #     "git+https://github.com/ast-grep/ast-grep-mcp"
-    #     "ast-grep-server"
-    #   ];
-    #   env = {
-    #   };
-    # };
-    # };
-  };
+    hooks = {
+      # Notification = lib.optionals pkgs.stdenv.isDarwin [
+      #   {
+      #     matcher = "";
+      #     hooks = [
+      #       {
+      #         type = "command";
+      #         command = "afplay /System/Library/Sounds/Blow.aiff";
+      #       }
+      #     ];
+      #   }
+      # ];
+      # Stop = lib.optionals pkgs.stdenv.isDarwin [
+      #   {
+      #     matcher = "";
+      #     hooks = [
+      #       {
+      #         type = "command";
+      #         command = "afplay /System/Library/Sounds/Hero.aiff";
+      #       }
+      #     ];
+      #   }
+      # ];
 
-  # TODO: https://docs.anthropic.com/en/docs/claude-code/settings
-  # home.file = {
-  #   ".claude/settings.json".text = builtins.toJSON {
-  #     statusLine = {
-  #       type = "command";
-  #       command = "bunx ccusage statusline";
-  #     };
-  #   };
-  # };
+      Notification = lib.optionals pkgs.stdenv.isDarwin ''
+        #!/usr/bin/env bash
+        afplay /System/Library/Sounds/Blow.aiff
+      '';
+      Stop = lib.optionals pkgs.stdenv.isDarwin ''
+        #!/usr/bin/env bash
+        afplay /System/Library/Sounds/Hero.aiff
+      '';
+    };
+  };
 }
