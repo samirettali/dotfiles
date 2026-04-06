37bf2e473fec17519cae4bb47a7ce67b4b4389f8
Samir Ettali <ettali.samir@gmail.com>
2026-04-06 22:31:31 +0200

    chore(hammerspoon): change alert style

--- home/dotfiles/hammerspoon/hammerspoon_config.lua
+++ home/dotfiles/hammerspoon/hammerspoon_config.lua
@@ -1,18 +1,17 @@
-local lib = require("lib")
-
-local alertStyle = {
-	strokeWidth = 8,
+local defaultStyle = {
+	strokeWidth = 4,
 	strokeColor = { white = 1, alpha = 1 },
 	fillColor = { white = 0, alpha = 1 },
 	textColor = { white = 1, alpha = 1 },
 	textFont = "JetBrainsMono Nerd Font",
-	textSize = 20,
+	textSize = 19,
+	radius = 27,
+	atScreenEdge = 0,
+	fadeInDuration = 0.0,
+	fadeOutDuration = 0.0,
 	padding = 30,
-	radius = 16,
-	fadeInDuration = 0,
-	fadeOutDuration = 0,
 }
 
-hs.alert.defaultStyle = lib.merge_tables(hs.alert.defaultStyle, alertStyle)
+hs.alert.defaultStyle = defaultStyle
 
 hs.window.animationDuration = 0
