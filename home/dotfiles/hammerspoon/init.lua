47492e982ae4e706f086cc024d5dd1a374120642
Samir Ettali <ettali.samir@gmail.com>
2026-04-06 22:31:37 +0200

    feat(hammerspoon): add seal spoon

--- home/dotfiles/hammerspoon/init.lua
+++ home/dotfiles/hammerspoon/init.lua
@@ -1,5 +1,9 @@
 local hs = hs -- TODO: check if this is needed
 
+if hs.fs.attributes("playground.lua") then
+	require("playground")
+end
+
 require("bindings")
 require("hammerspoon_config")
 require("recursive_binder")
@@ -7,6 +11,10 @@ require("recursive_binder")
 -- hs.loadSpoon("EmmyLua"):init() -- TODO: clone
 hs.loadSpoon("ControlEscape"):start()
 
-if hs.fs.attributes("playground.lua") then
-	require("playground")
-end
+seal = hs.loadSpoon("Seal")
+seal:loadPlugins({ "apps", "calc", "screencapture", "useractions" })
+seal:refreshAllCommands()
+seal:bindHotkeys({
+	show = { { "alt" }, "space" },
+})
+seal:start()
