72504a72dd9543aba6e469fa58173ea3ce62e031
Samir Ettali <ettali.samir@gmail.com>
2026-06-19 20:17:49 +0200

    chore(claude): remove hooks, set opus 4.8 as default model and set feedbackSurveyRate to 0

--- home/packages/shell/claude-code.nix
+++ home/packages/shell/claude-code.nix
@@ -7,7 +7,9 @@
   programs.claude-code = {
     enable = lib.mkDefault true;
     settings = {
+      model = "claude-opus-4-8";
       includeCoAuthoredBy = false;
+      feedbackSurveyRate = 0;
       env = {
         CLAUDE_CODE_IDE_SKIP_AUTO_INSTALL = "1";
         CLAUDE_CODE_DISABLE_NONESSENTIAL_TRAFFIC = "1";
@@ -17,39 +19,6 @@
         CLAUDE_CODE_DISABLE_FEEDBACK_SURVEY = "1";
         DISABLE_AUTOUPDATER = "1";
       };
-    };
-    hooks = {
-      # Notification = lib.optionals pkgs.stdenv.isDarwin [
-      #   {
-      #     matcher = "";
-      #     hooks = [
-      #       {
-      #         type = "command";
-      #         command = "afplay /System/Library/Sounds/Blow.aiff";
-      #       }
-      #     ];
-      #   }
-      # ];
-      # Stop = lib.optionals pkgs.stdenv.isDarwin [
-      #   {
-      #     matcher = "";
-      #     hooks = [
-      #       {
-      #         type = "command";
-      #         command = "afplay /System/Library/Sounds/Hero.aiff";
-      #       }
-      #     ];
-      #   }
-      # ];
-
-      Notification = lib.optionals pkgs.stdenv.isDarwin ''
-        #!/usr/bin/env bash
-        afplay /System/Library/Sounds/Blow.aiff
-      '';
-      Stop = lib.optionals pkgs.stdenv.isDarwin ''
-        #!/usr/bin/env bash
-        afplay /System/Library/Sounds/Hero.aiff
-      '';
     };
   };
 }
