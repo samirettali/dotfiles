50f1164536b631ad2a21ef33857785cb43fa5bb2
Samir Ettali <samir@ettali.com>
2026-06-19 23:25:58 +0200

    refactor(difftastic): enable git integration

--- home/packages/shell/difftastic.nix
+++ home/packages/shell/difftastic.nix
@@ -1,7 +1,6 @@
 {...}: {
   programs.difftastic = {
     enable = true;
-    # Wired into lazygit only (see lazygit.nix); leave plain `git diff` untouched.
-    git.enable = false;
+    git.enable = true;
   };
 }
