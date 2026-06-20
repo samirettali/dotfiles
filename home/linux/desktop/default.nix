0f8317221fd209349d8a9f3319430d71c9695d9b
Samir Ettali <samir@ettali.com>
2026-06-20 03:28:21 +0200

    refactor(chromium): move to its own file

--- home/linux/desktop/default.nix
+++ home/linux/desktop/default.nix
@@ -1,5 +1,6 @@
 {pkgs, ...}: {
   imports = [
+    ./chromium.nix
     ./wm
   ];
 
@@ -11,49 +12,4 @@
     ledger-live-desktop
     lmstudio
   ];
-
-  # TODO:
-  # - use ungoogled-chromium
-  # - disable smooth scroll
-  # - add custom search engines
-  #     - @nix https://mynixos.com/search?q=%s
-  #     - @gh https://github.com/search?q=%s
-  #     - @cl https://claude.ai/new?q=%s
-  #     - @pl https://perplexity.ai/?q=%s
-  programs.chromium = {
-    enable = true;
-    # package = pkgs.ungoogled-chromium;
-    extensions = [
-      {id = "cjpalhdlnbpafiamejdnhcphjbkeiagm";} # ublock
-      {id = "dbepggeogbaibhgnhhndojpepiihcmeb";} # vimium
-      {id = "oboonakemofpalcgghocfoadofidjkkk";} # keepassxc
-      {id = "niloccemoadcdkdjlinkgdfekeahmflj";} # pocket
-      {id = "gbmdgpbipfallnflgajpaliibnhdgobh";} # json viewer
-      {id = "mnjggcdmjocbbbhaepdhchncahnbgone";} # sponsorship block
-      {id = "acmacodkjbdgmoleebolmdjonilkdbch";} # rabby
-      {id = "bfnaelmomeimhlpmgjnjophhpkkoljpa";} # phantom
-      {id = "hnfanknocfeofbddgcijnmhnfnkdnaad";} # coinbase
-      {id = "bfnaelmomeimhlpmgjnjophhpkkoljpa";} # phantom
-      {id = "dmkamcknogkgcdfhhbddcghachkejeap";} # keplr
-    ];
-    # TODO: fix
-    # extraOpts = {
-    #   ShowHomeButton = false;
-    #   RestoreOnStartup = 1;
-    #   PasswordManagerEnabled = false;
-    #   SpellcheckEnabled = true;
-    #   SpellcheckLanguages = [
-    #     "en-US"
-    #     "it"
-    #   ];
-    #   SiteSearchSettings = [
-    #     {
-    #       featured = true;
-    #       name = "nix";
-    #       shortcut = "nix";
-    #       url = "https://mynixos.com/search?q=%s";
-    #     }
-    #   ];
-    # };
-  };
 }
