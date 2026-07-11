6c9a18c927efcb190204ea80b55f904671ceb1d9
Samir Ettali <samir@ettali.com>
2026-07-11 03:04:56 +0200

    refactor(nvim): colocate treesitter autocmds

--- home/dotfiles/nvim/lua/autocmds/init.lua
+++ home/dotfiles/nvim/lua/autocmds/init.lua
@@ -1,15 +1,3 @@
 require("autocmds.general")
 require("autocmds.lsp")
 require("autocmds.miscellaneous")
-
-vim.api.nvim_create_autocmd("PackChanged", {
-	callback = function(ev)
-		local name, kind = ev.data.spec.name, ev.data.kind
-		if name == "nvim-treesitter" and kind == "update" then
-			if not ev.data.active then
-				vim.cmd.packadd("nvim-treesitter")
-			end
-			vim.cmd("TSUpdate")
-		end
-	end,
-})
--- home/dotfiles/nvim/lua/autocmds/treesitter.lua
+++ home/dotfiles/nvim/lua/autocmds/treesitter.lua
@@ -1,6 +1,22 @@
+local group = vim.api.nvim_create_augroup("Treesitter", { clear = true })
+
 vim.api.nvim_create_autocmd("FileType", {
+	group = group,
 	pattern = "*",
 	callback = function(args)
 		pcall(vim.treesitter.start, args.buf)
+	end,
+})
+
+vim.api.nvim_create_autocmd("PackChanged", {
+	group = group,
+	callback = function(ev)
+		local name, kind = ev.data.spec.name, ev.data.kind
+		if name == "nvim-treesitter" and kind == "update" then
+			if not ev.data.active then
+				vim.cmd.packadd("nvim-treesitter")
+			end
+			vim.cmd("TSUpdate")
+		end
 	end,
 })
