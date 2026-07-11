9d9d6bb41b9db4c09cf490285a51563197c279bb
Samir Ettali <samir@ettali.com>
2026-07-11 03:03:48 +0200

    refactor(nvim): extract editor options

--- home/dotfiles/nvim/init.lua
+++ home/dotfiles/nvim/init.lua
@@ -21,44 +21,7 @@ require("plugins.nvim-web-devicons")
 
 vim.cmd("packadd nvim.undotree") -- built in
 
-vim.opt.tabstop = 4
-vim.opt.shiftwidth = 0
-vim.opt.expandtab = true
-vim.opt.smartindent = true
-vim.opt.ignorecase = true
-vim.opt.smartcase = true
-vim.opt.undofile = true
-vim.opt.swapfile = false
-vim.opt.autoread = true
-vim.opt.scrolloff = 5
-vim.opt.sidescrolloff = 5
-vim.opt.splitbelow = true
-vim.opt.splitright = true
-vim.opt.number = true
-vim.opt.relativenumber = true
-vim.opt.laststatus = 3
-vim.opt.cmdheight = 1
-vim.opt.clipboard = "unnamedplus"
-vim.opt.signcolumn = "yes"
--- vim.opt.diffopt = "internal,filler,closeoff,algorithm:histogram,context:5,linematch:60" -- TODO: read docs
--- vim.opt.statusline = '[%n] %<%f %h%w%m%r%=%-14.(%l,%c%V%) %P' -- https://github.com/y9san9/y9san9.nvim/blob/main/init.lua
-vim.opt.completeopt = {
-	"menu",
-	"menuone",
-	"noselect",
-	"noinsert",
-	"fuzzy",
-	"popup",
-}
-vim.opt.path:append("**")
-vim.opt.wildmenu = true
-vim.opt.wildmode = "longest:full,full"
-vim.opt.grepprg = "rg --vimgrep"
-vim.opt.shortmess:append("c")
-vim.opt.shortmess:append("I")
-vim.opt.fillchars = { eob = " " }
-vim.opt.winborder = "rounded"
-vim.opt.pumborder = "rounded"
+require("options")
 
 require("ui.tabline")
 require("autocmds")
