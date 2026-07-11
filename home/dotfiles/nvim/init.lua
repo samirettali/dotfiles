be401df9f3b0eeb091cc5959f2465baf04cbf3dd
Samir Ettali <samir@ettali.com>
2026-07-11 03:04:17 +0200

    refactor(nvim): extract global keymaps

--- home/dotfiles/nvim/init.lua
+++ home/dotfiles/nvim/init.lua
@@ -35,35 +35,7 @@ require("plugins")
 
 require("commands")
 
--- keymaps
-vim.keymap.set("n", "<leader>g", "<cmd>Grep <cword><cr>", { desc = "Grep word under cursor" })
-vim.keymap.set("n", "<leader>lq", vim.diagnostic.setqflist, { desc = "vim.diagnostic.setqflist()" })
-vim.keymap.set("n", "<leader>lc", vim.diagnostic.setloclist, { desc = "vim.diagnostic.setloclist()" })
-
--- nnoremap <silent><esc><esc> :nohlsearch<CR>
-vim.keymap.set("n", "<esc>", "<cmd>nohlsearch<cr>", { desc = "Clear search highlights" })
-
-vim.keymap.set("n", "<leader>ta", function()
-	vim.lsp.enable("copilot", not vim.lsp.is_enabled("copilot"))
-end, { desc = "Toggle copilot lsp" })
-
-vim.keymap.set("n", "<leader>td", function()
-	vim.diagnostic.enable(not vim.diagnostic.is_enabled())
-end, { desc = "Toggle diagnostic" })
-
-vim.keymap.set("n", "<leader>tf", function()
-	vim.g.disable_autoformat = not vim.g.disable_autoformat
-end, { desc = "Toggle format on save" })
-
-vim.keymap.set("n", "<leader>ti", function()
-	vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled())
-end, { desc = "Toggle inlay hints" })
-
-vim.keymap.set("n", "<leader>tv", function()
-	vim.diagnostic.config({
-		virtual_lines = not vim.diagnostic.config().virtual_lines,
-	})
-end, { desc = "Toggle diagnostic virtual lines" })
+require("keymaps")
 
 ----------------- yooooooooooo
 -- vim.opt.wildmode = "noselect"
