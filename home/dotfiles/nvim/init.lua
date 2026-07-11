9b49d6e5550c0945b6da83d209d819770c50030a
Samir Ettali <samir@ettali.com>
2026-07-11 03:00:22 +0200

    refactor(nvim): isolate gitsigns configuration

--- home/dotfiles/nvim/init.lua
+++ home/dotfiles/nvim/init.lua
@@ -13,8 +13,8 @@ require("plugins.nvim-treesitter")
 require("plugins.nvim-treesitter-textobjects")
 require("plugins.oil")
 vim.pack.add({
-	{ src = "https://github.com/lewis6991/gitsigns.nvim" },
 })
+require("plugins.gitsigns")
 require("plugins.moonfly")
 vim.pack.add({
 	{ src = "https://github.com/nvim-tree/nvim-web-devicons" },
@@ -199,12 +199,6 @@ end
 -- plugins setup
 require("plugins")
 
-require("gitsigns").setup({
-	current_line_blame = true,
-	current_line_blame_opts = { delay = 0 },
-	attach_to_untracked = true,
-})
-
 -- autocmds
 vim.api.nvim_create_autocmd("VimResized", {
 	desc = "Automatically resize windows when terminal is resized",
@@ -303,47 +297,6 @@ vim.keymap.set("n", "<leader>tv", function()
 		virtual_lines = not vim.diagnostic.config().virtual_lines,
 	})
 end, { desc = "Toggle diagnostic virtual lines" })
-
-vim.keymap.set("n", "]c", function()
-	if vim.wo.diff then
-		vim.cmd.normal({ "]c", bang = true })
-	else
-		require("gitsigns").nav_hunk("next")
-	end
-end)
-
-vim.keymap.set("n", "[c", function()
-	if vim.wo.diff then
-		vim.cmd.normal({ "[c", bang = true })
-	else
-		require("gitsigns").nav_hunk("prev")
-	end
-end)
-
-vim.keymap.set("n", "<leader>hs", require("gitsigns").stage_hunk, { desc = "Git: stage hunk" })
-vim.keymap.set("n", "<leader>hr", require("gitsigns").reset_hunk, { desc = "Git: reset hunk" })
-vim.keymap.set("n", "<leader>hR", require("gitsigns").reset_buffer, { desc = "Git: reset buffer" })
-vim.keymap.set("n", "<leader>hu", require("gitsigns").undo_stage_hunk, { desc = "Git: undo stage hunk" })
-vim.keymap.set("n", "<leader>hp", require("gitsigns").preview_hunk, { desc = "Git: preview hunk" })
-vim.keymap.set("n", "<leader>hi", require("gitsigns").preview_hunk_inline, { desc = "Git: preview hunk inline" })
-vim.keymap.set("n", "<leader>hB", require("gitsigns").blame, { desc = "Git: blame file" })
-vim.keymap.set({ "o", "x" }, "ih", require("gitsigns").select_hunk, { desc = "inside a hunk" })
-
-vim.keymap.set("v", "<leader>hs", function()
-	require("gitsigns").stage_hunk({ vim.fn.line("."), vim.fn.line("v") })
-end, { desc = "Git: stage hunk (visual)" })
-
-vim.keymap.set("v", "<leader>hr", function()
-	require("gitsigns").reset_hunk({ vim.fn.line("."), vim.fn.line("v") })
-end, { desc = "Git: reset hunk (visual)" })
-
-vim.keymap.set("n", "<leader>hb", function()
-	require("gitsigns").blame_line({ full = true })
-end, { desc = "Git: blame line" })
-
-vim.keymap.set("n", "<leader>hq", function()
-	require("gitsigns").setqflist("all")
-end, { desc = "Git: send hunks to quickfix list" })
 
 ----------------- yooooooooooo
 -- vim.opt.wildmode = "noselect"
