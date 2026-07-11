b093d676740a6441141492bc3fd90ef86a5922b8
Samir Ettali <samir@ettali.com>
2026-07-11 03:03:54 +0200

    refactor(nvim): extract lsp configuration

--- home/dotfiles/nvim/init.lua
+++ home/dotfiles/nvim/init.lua
@@ -28,31 +28,7 @@ require("autocmds")
 require("plugins.nightfly")
 require("plugins.auto-dark-mode")
 
--- diagnostic and lsp
-vim.diagnostic.config({ signs = false })
-
-vim.lsp.enable({
-	"bashls",
-	"clangd",
-	"gopls",
-	"golangci_lint_ls",
-	"lua_ls",
-	"stylua",
-	"jsonls",
-	"ts_ls", -- TODO: try vtsls
-	"eslint",
-	"nixd",
-	"roslyn_ls",
-	"rust_analyzer",
-	"solidity_ls",
-	"yamlls",
-	"buf_ls",
-	"copilot",
-	"basedpyright",
-	"ruff",
-	"harper",
-	"dartls",
-})
+require("lsp")
 
 -- plugins setup
 require("plugins")
