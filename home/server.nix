09153a55d60eda05030dea5d4be00b6b532ca2fa
Samir Ettali <samir@ettali.com>
2026-07-16 22:28:08 +0200

    fix: import js in server.nix

--- home/server.nix
+++ home/server.nix
@@ -4,6 +4,7 @@
     ./options.nix
     ./packages/dev/c.nix
     ./packages/dev/go.nix
+    ./packages/dev/js.nix
     ./packages/dev/python.nix
     ./packages/shell
     ./packages/desktop/google-chrome.nix
