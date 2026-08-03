cfc87232087f642aad051e863de638cf0b8c2f4e
Samir Ettali <samir@ettali.com>
2026-08-04 01:42:14 +0200

    fix(obsidian): handle nested macOS app bundle

--- home/packages/desktop/obsidian.nix
+++ home/packages/desktop/obsidian.nix
@@ -1,6 +1,12 @@
-{...}: {
+{pkgs, ...}: {
   programs.obsidian = {
     enable = true;
     cli.enable = true;
+
+    # TODO: upstream broken
+    package = pkgs.obsidian.overrideAttrs (old:
+      pkgs.lib.optionalAttrs pkgs.stdenv.hostPlatform.isDarwin {
+        sourceRoot = "Obsidian ${old.version}-universal/Obsidian.app";
+      });
   };
 }
