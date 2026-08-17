876e08cae759d7cf8ac50c7ddd2c3794960b365f
Samir Ettali <samir@ettali.com>
2026-08-17 18:48:07 +0200

    chore(mcp): remove old servers

--- home/packages/ai/mcp.nix
+++ home/packages/ai/mcp.nix
@@ -10,33 +10,6 @@
           "--browserUrl=http://127.0.0.1:9222"
         ];
       };
-      #   everything = {
-      #     command = "npx";
-      #     args = [
-      #       "-y"
-      #       "@modelcontextprotocol/server-everything"
-      #     ];
-      #   };
-      #   context7 = {
-      #     url = "https://mcp.context7.com/mcp";
-      #     # headers = {
-      #     #   CONTEXT7_API_KEY = "{env:CONTEXT7_API_KEY}";
-      #     # };
-      #   };
-      #   playwright = {
-      #     command = "npx";
-      #     args = [
-      #       "@playwright/mcp@latest"
-      #     ];
-      #   };
-      #   ast-grep = {
-      #     command = "uvx";
-      #     args = [
-      #       "--from"
-      #       "git+https://github.com/ast-grep/ast-grep-mcp"
-      #       "ast-grep-server"
-      #     ];
-      #   };
     };
   };
 }
