79d326567adbed1b59365283fc858a815f5b2a72
Samir Ettali <samir@ettali.com>
2026-08-20 18:57:06 +0200

    chore(herdr): move config in nix store

--- home/packages/shell/herdr.nix
+++ home/packages/shell/herdr.nix
@@ -1,15 +1,16 @@
 {
   inputs,
+  config,
   nurPkgs,
+  lib,
   ...
 }: {
   home.packages = [
     (import ./herdr-package.nix {inherit inputs nurPkgs;})
   ];
 
-  # TODO
-  # xdg.configFile."herdr/config.toml" = {
-  #   enable = builtins.elem nurPkgs.herdr config.home.packages;
-  #   source = ../../dotfiles/herdr/config.toml;
-  # };
+  xdg.configFile."herdr/config.toml" = {
+    enable = lib.any (p: (p.pname or "") == "herdr") config.home.packages;
+    source = ../../dotfiles/herdr/config.toml;
+  };
 }
