bc124c8eba1a803ab36bbae3f7aa66d4d3393ac0
Samir Ettali <samir@ettali.com>
2026-09-18 12:56:55 +0200

    fix(nvim): follow upstream lspconfig in the server configs
    eslint got the workspace folder as a path where the protocol wants a
    URI. clangd and gdscript listed filetypes that no longer match what
    nvim sets. golangci_lint_ls probed the binary version before checking
    the binary is there.
    
    buf_ls gains the buf-config filetype and reuse_client. The filetype
    needs a vim.filetype.add rule to ever be set, which this does not add.

--- home/dotfiles/nvim/lsp/gdscript.lua
+++ home/dotfiles/nvim/lsp/gdscript.lua
@@ -8,6 +8,6 @@
 ---@type vim.lsp.Config
 return {
 	cmd = vim.lsp.rpc.connect("127.0.0.1", 6005),
-	filetypes = { "gd", "gdscript", "gdscript3" },
+	filetypes = { "gdscript" },
 	root_markers = { "project.godot", ".git" },
 }
