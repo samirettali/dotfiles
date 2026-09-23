09dd4bf3c5a2e94413a66ec00b84861e146bcfed
Samir Ettali <samir@ettali.com>
2026-09-23 03:12:36 +0200

    chore(nh): drop the weekly clean agent
    It failed on every run: home-manager passes the string --keep 1 to nh as one argument.

--- home/packages/shell/nh.nix
+++ home/packages/shell/nh.nix
@@ -1,10 +1,6 @@
 {config, ...}: {
   programs.nh = {
     enable = true;
-    clean = {
-      enable = true;
-      extraArgs = "--keep 1";
-    };
     flake = config.home.homeDirectory + "/dev/dotfiles"; # TODO: is there a better way to do this?
   };
 }
