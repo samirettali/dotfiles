c538735f5b9eae7a5b05c641adcfa42c78604ef9
Samir Ettali <samir@ettali.com>
2026-09-23 02:59:22 +0200

    chore(nvim): delete dead code
    The snippets needed luasnip, which is not installed (gopls completes .iferr itself); the sql formatter was disabled by an early return; the tabline named plugins no longer installed; P() duplicated vim.print.

--- home/dotfiles/nvim/init.lua
+++ home/dotfiles/nvim/init.lua
@@ -2,11 +2,6 @@ vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 
 -- functions
-function P(x)
-	print(vim.inspect(x))
-	return x
-end
-
 function N(x, level, opts)
 	vim.notify(vim.inspect(x), level, opts)
 	return x
@@ -32,24 +27,3 @@ require("plugins")
 require("commands")
 
 require("keymaps")
-
------------------ yooooooooooo
--- vim.opt.wildmode = "noselect"
--- vim.api.nvim_create_autocmd("CmdlineChanged", {
--- 	pattern = ":",
--- 	callback = function()
--- 		vim.fn.wildtrigger()
--- 	end,
--- })
-
--- function _G.my_find(text, _)
--- 	local files = vim.fn.glob("**/*", true, true)
--- 	-- return { "%#DiagnosticError#" .. text .. "%*" .. "\t" .. "%#DiagnosticHint#" .. "in " .. files }
--- 	local result = vim.fn.matchfuzzy(files, text)
--- 	P(result)
--- 	return result
--- end
---
--- vim.opt.findfunc = "v:lua.my_find"
-
--------------------------------------
